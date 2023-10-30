6fc3beb2c295ab2f674928f28965c6cb9860d2b5
Kimdaeun <104294861+GBAJS754@users.noreply.github.com>
2023-10-30 18:27:56 +0900

    [OMCT-68] commonTabs 컴포넌트 구현 (#11)
    * feat: #68 commonTabs 컴포넌트 구현
    
    * feat : commonTabs onClick 추가
    
    * refactor: 함수명 수정

---
+++ src/components/common/Tabs/index.tsx
@@ -0,0 +1,49 @@
+import { ReactElement } from 'react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel, TabsProps } from '@chakra-ui/react';
+
+interface TabsData {
+  label: string;
+  content: string | ReactElement;
+}
+interface CommonTabsProps {
+  tabsType?: 'soft-rounded' | 'line';
+  isFitted?: TabsProps['isFitted'];
+  tabsData: TabsData[];
+  onClick?: () => void;
+}
+
+const CommonTabs = ({ tabsData, isFitted = true, tabsType = 'line', onClick }: CommonTabsProps) => {
+  const handleClick = () => {
+    onClick && onClick();
+  };
+  return (
+    <Tabs isFitted={isFitted} variant={tabsType} size="sm">
+      <TabList>
+        {tabsData.map((tab, index) => (
+          <Tab
+            onClick={handleClick}
+            color="blue.900"
+            bg="none"
+            _selected={
+              tabsType === 'line'
+                ? { color: 'blue.300', borderColor: 'blue.300' }
+                : { color: 'white', bg: 'blue.300' }
+            }
+            key={index}
+          >
+            {tab.label}
+          </Tab>
+        ))}
+      </TabList>
+      <TabPanels>
+        {tabsData.map((tab, index) => (
+          <TabPanel p={4} key={index}>
+            {tab.content}
+          </TabPanel>
+        ))}
+      </TabPanels>
+    </Tabs>
+  );
+};
+
+export default CommonTabs;
