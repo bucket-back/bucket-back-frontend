08676f6925d7d82632e1f2afbb30103ae66d0bbf
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-10-30 18:29:57 +0900

    [Omct 75] 브라우저에 Service worker 등록 (#13)
    * init: browser에 service worker 등록
    
    * chorm: pakage.json에 workerDirectory 추가
    
    ---------
    
    Co-authored-by: kim <perfumeplaylist.gmail.com>
    Co-authored-by: chan uk jeong <40534414+seeyoujeong@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
+async function deferRender() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  return worker.start();
+}
+
+deferRender().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  );
+});
