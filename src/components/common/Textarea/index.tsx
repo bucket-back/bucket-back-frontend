03dbc6d494eefe587b79cbb2e526d8bc69a2ec12
daeun <gbajs754@gmail.com>
2023-11-01 01:59:17 +0900

    feat: CommonTextarea 컴포넌트 구현

---
+++ src/components/common/Textarea/index.tsx
@@ -0,0 +1,47 @@
+import { FormControl, Textarea, FormErrorMessage, FormLabel } from '@chakra-ui/react';
+import { useFormContext, RegisterOptions } from 'react-hook-form';
+
+interface CommonTextareaProps {
+  placeholder: string;
+  name: string;
+  registerOptions: RegisterOptions;
+  label?: string;
+  size: 'xs' | 'sm' | 'base';
+}
+
+const TextareaSize = {
+  xs: { width: '20.5rem', height: '5.8125rem' },
+  sm: { width: '21.9375rem', height: '9.875rem' },
+  base: { width: '21.9375rem', height: '14.375rem' },
+};
+
+const CommonTextarea = ({
+  placeholder,
+  name,
+  label,
+  registerOptions,
+  size,
+}: CommonTextareaProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <FormControl isInvalid={!!errors[name]} isRequired={!!registerOptions.required}>
+      <FormLabel>{label}</FormLabel>
+      <Textarea
+        id={name}
+        size="sm"
+        isInvalid={!!errors[name]}
+        _focusVisible={{ boxShadow: 'none', outline: 'none' }}
+        placeholder={placeholder}
+        {...TextareaSize[size]}
+        {...register(name, registerOptions)}
+      />
+      {errors[name] && <FormErrorMessage>{errors[name]?.message as string}</FormErrorMessage>}
+    </FormControl>
+  );
+};
+
+export default CommonTextarea;
