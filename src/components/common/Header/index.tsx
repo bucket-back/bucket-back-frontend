4ec1cfd39857515f9b770c72870f0be616fddfaf
daeun <gbajs754@gmail.com>
2023-11-01 18:37:13 +0900

    refactor: 변수명 재수정

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 const Header = ({ type }: HeaderProps) => {
   const navigate = useNavigate();
 
-  const HEADER_TYPE = {
+  const headerType = {
     logo: (
       <Text fontSize="2xl" as="b" color="blue.900">
         버킷백
@@ -20,7 +20,7 @@ const Header = ({ type }: HeaderProps) => {
 
   return (
     <Box height="5rem" display="flex" alignItems="center" pl="1.75rem" pos="fixed" width="full">
-      {HEADER_TYPE[type]}
+      {headerType[type]}
     </Box>
   );
 };
