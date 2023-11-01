4fda506f8d5bcad01ccb6795b278ed46d82d35a1
kim <perfumeplaylist.gmail.com>
2023-11-01 17:10:39 +0900

    fix: onClick 함수 추가

--- src/components/common/Avatar/index.tsx
+++ src/components/common/Avatar/index.tsx
@@ -5,25 +5,34 @@ interface CommonAvatar {
   isOwner: boolean;
   size: string;
   src: AvatarProps['src'];
+  onClick: () => void;
 }
 
 const CommonAvatar = ({
   isOwner = false,
   size,
   src = 'https://bit.ly/broken-link',
+  onClick,
 }: CommonAvatar) => {
   return (
     <>
       {isOwner ? (
         <Box position="relative" width="8em">
           <Avatar src={src} size="8rem" />
-          <Circle size="8" bg="gray.100" position="absolute" top="6.165rem" right="0.45rem">
+          <Circle
+            size="8"
+            bg="gray.100"
+            position="absolute"
+            top="6.165rem"
+            right="0.45rem"
+            onClick={() => onClick()}
+          >
             <FaPen />
           </Circle>
         </Box>
       ) : (
         <Box>
-          <Avatar src={src} width={size} height={size} />
+          <Avatar src={src} width={size} height={size} onClick={() => onClick()} />
         </Box>
       )}
     </>
