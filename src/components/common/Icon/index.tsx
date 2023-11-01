314bf311ccb95a9e4a1cfad5b3a41a0cda7ba22c
chan uk jeong <40534414+seeyoujeong@users.noreply.github.com>
2023-11-01 17:40:53 +0900

    [OMCT-107] CommonIcon 컴포넌트 구현 (#22)
    * feat: CommonIcon 컴포넌트 구현
    
    * refactor: icons 변수명 수정

---
+++ src/components/common/Icon/index.tsx
@@ -0,0 +1,74 @@
+import { Icon } from '@chakra-ui/react';
+import {
+  FaRegTrashCan,
+  FaChevronLeft,
+  FaChevronRight,
+  FaChevronDown,
+  FaHeart,
+  FaRegHeart,
+  FaMagnifyingGlass,
+  FaPen,
+  FaCube,
+  FaCubes,
+  FaBasketShopping,
+  FaBagShopping,
+  FaRegFaceGrin,
+  FaEye,
+  FaEyeSlash,
+  FaArrowUp,
+  FaPlus,
+  FaRegStar,
+  FaStar,
+  FaRegCommentDots,
+  FaCircleExclamation,
+  FaCircleCheck,
+  FaRegCircleXmark,
+  FaEllipsisVertical,
+  FaCrown,
+  FaHouse,
+  FaBriefcase,
+  FaRegRectangleList,
+} from 'react-icons/fa6';
+
+const ICONS = {
+  trashcan: FaRegTrashCan,
+  chevronLeft: FaChevronLeft,
+  chevronRight: FaChevronRight,
+  chevronDown: FaChevronDown,
+  heart: FaRegHeart,
+  fillHeart: FaHeart,
+  search: FaMagnifyingGlass,
+  pen: FaPen,
+  cube: FaCube,
+  cubes: FaCubes,
+  basket: FaBasketShopping,
+  bag: FaBagShopping,
+  my: FaRegFaceGrin,
+  eye: FaEye,
+  eyeSlash: FaEyeSlash,
+  arrowUp: FaArrowUp,
+  plus: FaPlus,
+  star: FaRegStar,
+  fillStar: FaStar,
+  comment: FaRegCommentDots,
+  circleExclamation: FaCircleExclamation,
+  circleCheck: FaCircleCheck,
+  circleXmark: FaRegCircleXmark,
+  ellipsis: FaEllipsisVertical,
+  crown: FaCrown,
+  home: FaHouse,
+  inventory: FaBriefcase,
+  feed: FaRegRectangleList,
+};
+
+interface CommonIconProps {
+  type: keyof typeof ICONS;
+  size?: string;
+  color?: string;
+}
+
+const CommonIcon = ({ type, size, color }: CommonIconProps) => {
+  return <Icon as={ICONS[type]} boxSize={size} color={color} />;
+};
+
+export default CommonIcon;
