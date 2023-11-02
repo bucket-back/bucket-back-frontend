e108c89ad0f8d622e6b7d12cb8f8690d7e9cf020
chan uk jeong <40534414+seeyoujeong@users.noreply.github.com>
2023-11-02 17:28:33 +0900

    [OMCT-109] CommonIconButton 컴포넌트 구현 (#24)
    * feat: CommonIconButton 컴포넌트 구현
    
    * feat: 뒤로가기 아이콘버튼 추가
    
    * refactor: width, height, fontSize 전달할 수 있게 수정
    
    * refactor: modify를 update로 수정
    
    * refactor: width, height, fontSize 타입 수정

---
+++ src/components/common/IconButton/index.tsx
@@ -0,0 +1,104 @@
+import { IconButton } from '@chakra-ui/react';
+import CommonIcon from '../Icon';
+
+interface CommonIconButtonProps {
+  type: 'delete' | 'create' | 'up' | 'update' | 'add' | 'detail' | 'back';
+  width?: `${number}rem`;
+  height?: `${number}rem`;
+  fontSize?: `${number}rem`;
+  onClick: () => void;
+}
+
+const CommonIconButton = ({ type, width, height, fontSize, onClick }: CommonIconButtonProps) => {
+  const iconButton = {
+    delete: (
+      <IconButton
+        variant="unstyled"
+        aria-label={`${type}`}
+        w={width || '2.5rem'}
+        h={height || '2.5rem'}
+        fontSize={fontSize || '1rem'}
+        icon={<CommonIcon type="trashcan" />}
+        onClick={onClick}
+      />
+    ),
+    create: (
+      <IconButton
+        isRound
+        color="white"
+        bgColor="blue.300"
+        w={width || '4.0625rem'}
+        h={height || '4.0625rem'}
+        fontSize={fontSize || '1.4375rem'}
+        aria-label={`${type}`}
+        icon={<CommonIcon type="pen" />}
+        onClick={onClick}
+      />
+    ),
+    up: (
+      <IconButton
+        isRound
+        color="white"
+        bgColor="gray.700"
+        w={width || '2.5rem'}
+        h={height || '2.5rem'}
+        fontSize={fontSize || '1rem'}
+        aria-label={`${type}`}
+        icon={<CommonIcon type="arrowUp" />}
+        onClick={onClick}
+      />
+    ),
+    update: (
+      <IconButton
+        variant="unstyled"
+        aria-label={`${type}`}
+        w={width || '2.5rem'}
+        h={height || '2.5rem'}
+        fontSize={fontSize || '1rem'}
+        icon={<CommonIcon type="pen" />}
+        onClick={onClick}
+      />
+    ),
+    add: (
+      <IconButton
+        isRound
+        color="white"
+        bgColor="blue.300"
+        w={width || '4.0625rem'}
+        h={height || '4.0625rem'}
+        fontSize={fontSize || '1.4375rem'}
+        aria-label={`${type}`}
+        icon={<CommonIcon type="plus" />}
+        onClick={onClick}
+      />
+    ),
+    detail: (
+      <IconButton
+        variant="unstyled"
+        aria-label={`${type}`}
+        w={width || '2.5rem'}
+        h={height || '2.5rem'}
+        fontSize={fontSize || '1.5rem'}
+        display="flex"
+        icon={<CommonIcon type="chevronRight" />}
+        onClick={onClick}
+      />
+    ),
+    back: (
+      <IconButton
+        variant="unstyled"
+        aria-label={`${type}`}
+        w={width || '2.5rem'}
+        h={height || '2.5rem'}
+        fontSize={fontSize || '1.25rem'}
+        display="flex"
+        icon={<CommonIcon type="chevronLeft" />}
+        onClick={onClick}
+      />
+    ),
+  };
+
+  return <>{iconButton[type]}</>;
+};
+
+export default CommonIconButton;
