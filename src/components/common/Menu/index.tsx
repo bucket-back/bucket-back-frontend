e00bcc802f1abf15606c19469fe11140772ddf30
chan uk jeong <40534414+seeyoujeong@users.noreply.github.com>
2023-11-07 14:52:06 +0900

    [OMCT-104] CommonMenu 컴포넌트 구현 (#25)
    * feat: CommonMenu 컴포넌트 구현
    
    * refactor: onDelete를 필수로 수정
    
    * refactor: iconSize, fontSize props 추가
    
    * refactor: menus 중복 제거
    
    * refactor: handle 함수들 삭제 후 handleClick 함수 추가

---
+++ src/components/common/Menu/index.tsx
@@ -0,0 +1,51 @@
+import { IconButton, Menu, MenuButton, MenuList, MenuItem, MenuDivider } from '@chakra-ui/react';
+import CommonIcon from '../Icon';
+
+interface CommonMenuProps {
+  type: 'update' | 'logout' | 'create';
+  iconSize?: `${number}rem`;
+  fontSize?: `${number}rem`;
+  onUpdate?: () => void;
+  onDelete: () => void;
+  onLogout?: () => void;
+  onCreate?: () => void;
+}
+
+const CommonMenu = ({
+  type,
+  iconSize = '1.25rem',
+  fontSize = '1rem',
+  onUpdate,
+  onDelete,
+  onLogout,
+  onCreate,
+}: CommonMenuProps) => {
+  const handleClick = (onEvent?: () => void) => {
+    onEvent && onEvent();
+  };
+
+  const menus = {
+    update: <MenuItem onClick={() => handleClick(onUpdate)}>수정</MenuItem>,
+    logout: <MenuItem onClick={() => handleClick(onLogout)}>로그아웃</MenuItem>,
+    create: <MenuItem onClick={() => handleClick(onCreate)}>생성</MenuItem>,
+  };
+
+  return (
+    <Menu autoSelect={false}>
+      <MenuButton
+        as={IconButton}
+        display="flex"
+        fontSize={iconSize}
+        icon={<CommonIcon type="ellipsis" />}
+        variant="unstyled"
+      />
+      <MenuList p="0" minW="0" w="6rem" fontSize={fontSize}>
+        {menus[type]}
+        <MenuDivider m="0" />
+        <MenuItem onClick={onDelete}>{type === 'logout' ? '탈퇴' : '삭제'}</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default CommonMenu;
