32885855c12700b1271951c69bf3de39e0761efb
Kimdaeun <104294861+GBAJS754@users.noreply.github.com>
2023-11-07 14:51:15 +0900

    [OMCT-114] CommonTextarea 컴포넌트 구현 (#21)
    * feat: CommonTextarea 컴포넌트 구현
    
    * refactor: 변수명 수정
    
    * refactor: 리액트훅폼 종속성 제거

---
+++ src/components/common/Textarea/index.tsx
@@ -0,0 +1,35 @@
+import { FormControl, Textarea, FormErrorMessage, FormLabel, forwardRef } from '@chakra-ui/react';
+import { FieldError } from 'react-hook-form';
+
+interface CommonTextareaProps {
+  placeholder: string;
+  label?: string;
+  error?: FieldError;
+  size: keyof typeof TEXTAREA_SIZE;
+}
+
+const TEXTAREA_SIZE = {
+  xs: { width: '20.5rem', height: '5.8125rem' },
+  sm: { width: '21.9375rem', height: '9.875rem' },
+  base: { width: '21.9375rem', height: '14.375rem' },
+};
+
+const CommonTextarea = forwardRef(
+  ({ placeholder, label, size, error, ...props }: CommonTextareaProps, ref) => {
+    return (
+      <FormControl isInvalid={Boolean(error?.message)}>
+        <FormLabel>{label}</FormLabel>
+        <Textarea
+          size="sm"
+          placeholder={placeholder}
+          ref={ref}
+          {...TEXTAREA_SIZE[size]}
+          {...props}
+        />
+        {error?.message && <FormErrorMessage>{error.message}</FormErrorMessage>}
+      </FormControl>
+    );
+  }
+);
+
+export default CommonTextarea;
