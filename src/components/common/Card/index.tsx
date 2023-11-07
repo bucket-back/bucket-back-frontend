0765860d478d05cfd4272ae068f4d4f59b0fce6c
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-11-07 16:07:47 +0900

    feat: CommonCard 컴포넌트 구현 (#29)
    Co-authored-by: kim <perfumeplaylist.gmail.com>

---
+++ src/components/common/Card/index.tsx
@@ -0,0 +1,27 @@
+import { Card, CardHeader, CardBody } from '@chakra-ui/react';
+import { ReactElement } from 'react';
+import CommonBadge from '../Badge';
+
+interface CommonCardProps {
+  count: number;
+  children: ReactElement;
+  onClick: () => void;
+}
+
+const CommonCard = ({ count, children, onClick }: CommonCardProps) => {
+  return (
+    <Card
+      onClick={onClick}
+      maxW="24.5rem"
+      minH="16.1875rem"
+      boxShadow="0px 1px 2px 0px rgba(0, 0, 0, 0.06), 0px 1px 3px 0px rgba(0, 0, 0, 0.10)"
+    >
+      <CardHeader p="0">
+        <CommonBadge type="vote" count={count} />
+      </CardHeader>
+      <CardBody px="1.56rem">{children}</CardBody>
+    </Card>
+  );
+};
+
+export default CommonCard;
