3b832dda97726f862b98083e366775c68a212089
Kimdaeun <104294861+GBAJS754@users.noreply.github.com>
2023-11-07 15:05:58 +0900

    [OMCT-69] CommonToast 컴포넌트 구현 (#33)
    * feat: CommonToast 컴포넌트, 훅 구현
    
    * refactor: 단위수정,객체반환 수정

---
+++ src/components/common/Toast/index.tsx
@@ -0,0 +1,35 @@
+import { Flex } from '@chakra-ui/react';
+import CommonIcon from '../Icon';
+import { OpenToastProps } from '../../../hooks/useCustomToast';
+import CommonText from '../Text';
+
+const CommonToast = ({ message, type }: OpenToastProps) => {
+  return (
+    <>
+      <Flex
+        borderRadius="0.625rem"
+        alignItems="center"
+        justifyContent="center"
+        color="blue.900"
+        p="0.69rem"
+        bg="blue.100"
+        gap="0.5rem"
+      >
+        {type === 'success' ? (
+          <CommonIcon type="circleCheck" size="1.5rem" color="green.400" />
+        ) : (
+          <CommonIcon
+            type="circleExclamation"
+            size="1.5rem"
+            color={type === 'error' ? 'red.400' : 'blue.800'}
+          />
+        )}
+        <CommonText type="normalInfo" color="blue.900" noOfLines={0}>
+          {message}
+        </CommonText>
+      </Flex>
+    </>
+  );
+};
+
+export default CommonToast;
