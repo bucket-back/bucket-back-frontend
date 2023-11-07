d9d51e2df78db1b44cc6ea0264775caa4624053f
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-11-07 14:53:03 +0900

    [OMCT-102] CommonRadio 컴포넌트 제작 (#27)
    * feat: CommonRadio 컴포넌트 구현
    
    * fix: CommonRadio 컴포넌트 props 수정
    
    * fix: 컨벤셜에 맞는 코드 수정/ CommonText 추가
    
    ---------
    
    Co-authored-by: kim <perfumeplaylist.gmail.com>

---
+++ src/components/common/Radio/RadioCard.tsx
@@ -0,0 +1,33 @@
+import { Box, useRadio, RadioProps } from '@chakra-ui/react';
+import CommonText from '../Text';
+
+const RadioCard = (props: RadioProps) => {
+  const { getInputProps, getRadioProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getRadioProps();
+
+  return (
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        borderWidth="0.024rem"
+        borderRadius="0.375rem"
+        _checked={{
+          bg: 'blue.300',
+          color: 'white',
+          borderColor: 'teal.600',
+        }}
+        p="0rem 1rem"
+      >
+        <CommonText type="strongInfo" color={input.checked ? 'white' : 'blue.300'} noOfLines={1}>
+          {String(props.value)}
+        </CommonText>
+      </Box>
+    </Box>
+  );
+};
+
+export default RadioCard;
