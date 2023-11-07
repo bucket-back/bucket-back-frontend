b2b43f758295417504168a680eabb62facdb013f
Kimdaeun <104294861+GBAJS754@users.noreply.github.com>
2023-11-07 15:07:37 +0900

    [OMCT-108] CommonInput 컴포넌트 구현 (#34)
    * feat: CommonInput 컴포넌트 구현
    
    * refactor: 삼항연산자 수정

---
+++ src/components/common/Input/index.tsx
@@ -0,0 +1,54 @@
+import {
+  FormControl,
+  Input,
+  FormErrorMessage,
+  InputGroup,
+  forwardRef,
+  FormLabel,
+  InputRightElement,
+  InputLeftElement,
+} from '@chakra-ui/react';
+import { ReactElement } from 'react';
+import { FieldError } from 'react-hook-form';
+
+interface CommonInputProps {
+  placeholder: string;
+  type: 'password' | 'text';
+  error?: FieldError;
+  label?: string;
+  leftIcon?: ReactElement;
+  rightIcon?: ReactElement;
+  size?: 'sm' | 'md' | 'lg';
+  width?: `${number}rem`;
+}
+
+const CommonInput = forwardRef(
+  (
+    {
+      placeholder,
+      error,
+      label,
+      rightIcon,
+      leftIcon,
+      type,
+      size = 'md',
+      width = '18.4375rem',
+      ...props
+    }: CommonInputProps,
+    ref
+  ) => {
+    return (
+      <FormControl isInvalid={Boolean(error?.message)}>
+        <FormLabel>{label}</FormLabel>
+        <InputGroup width={width} size={size}>
+          {leftIcon && <InputLeftElement>{leftIcon}</InputLeftElement>}
+          <Input placeholder={placeholder} ref={ref} {...props} type={type} />
+          {rightIcon && <InputRightElement>{rightIcon}</InputRightElement>}
+        </InputGroup>
+        {error?.message && <FormErrorMessage>{error.message}</FormErrorMessage>}
+      </FormControl>
+    );
+  }
+);
+
+export default CommonInput;
