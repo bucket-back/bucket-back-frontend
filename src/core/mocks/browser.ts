6547c71f8e52af198aa04e93d618cc538ab59084
kim <perfumeplaylist.gmail.com>
2023-11-08 16:49:58 +0900

    feat: item handler 작성

--- src/core/mocks/browser.ts
+++ src/core/mocks/browser.ts
@@ -1,5 +1,6 @@
 import { setupWorker } from 'msw/browser';
-import { handler as voteHandler } from './handler/vote';
 import { handler as feedHandler } from './handler/feed';
+import { handler as itemHandler } from './handler/item';
+import { handler as voteHandler } from './handler/vote';
 
-export const worker = setupWorker(...voteHandler, ...feedHandler);
+export const worker = setupWorker(...voteHandler, ...feedHandler, ...itemHandler);
