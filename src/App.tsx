2d720fedba7aebff366915a7a5a58861f2eec424
seeyoujeong <chan3358@naver.com>
2023-11-08 15:14:23 +0900

    design: App 레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import { Outlet } from 'react-router-dom';
+import styled from '@emotion/styled';
 
 const App = () => {
   return (
-    <div>
+    <AppContainer>
       <Outlet />
-    </div>
+    </AppContainer>
   );
 };
 
 export default App;
+
+const AppContainer = styled.div`
+  max-width: 26.875rem;
+  width: 100%;
+  height: 100vh;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: auto;
+`;
