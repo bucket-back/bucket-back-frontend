c4324ce6089d7a875373f1665d40e6ae26d10a27
seeyoujeong <chan3358@naver.com>
2023-11-11 20:20:41 +0900

    feat: FeedItem 컴포넌트 구현

---
+++ src/features/feed/components/FeedItem/style.tsx
@@ -0,0 +1,67 @@
+import styled from '@emotion/styled';
+import { COMMON } from '@/shared/styles/Common';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem 0;
+`;
+
+export const ProfileWrapper = styled.div`
+  padding: 0 1.5rem;
+`;
+
+export const ContentsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  padding: 0 2.5rem;
+`;
+
+export const ImageBox = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0.25rem;
+`;
+
+export const BucketTitleTag = styled.div`
+  position: absolute;
+  right: 0.6rem;
+  bottom: 0.6rem;
+`;
+
+export const DetailInfoWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 0.5rem;
+`;
+
+export const InteractPanel = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const LikeBox = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 0.2rem;
+  color: ${COMMON.COLORS.PLACEHOLDER_COLOR};
+`;
+
+export const LikeNumber = styled.span`
+  font-size: 0.75rem;
+  color: ${COMMON.COLORS.PLACEHOLDER_COLOR};
+`;
+
+export const CommentBox = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 0.2rem;
+  color: ${COMMON.COLORS.PLACEHOLDER_COLOR};
+`;
+
+export const CommentNumber = styled.span`
+  font-size: 0.75rem;
+`;
