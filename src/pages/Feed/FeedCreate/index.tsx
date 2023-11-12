aa1b44a12deaec1d4dfa5b6f9f5d654be25fe85a
seeyoujeong <chan3358@naver.com>
2023-11-12 23:46:17 +0900

    feat: useState, CommonDrawer 추가

--- src/pages/Feed/FeedCreate/index.tsx
+++ src/pages/Feed/FeedCreate/index.tsx
@@ -1,6 +1,14 @@
 import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { CommonButton, CommonRadio, CommonText, CommonTextarea, Header } from '@/shared/components';
+import { useDisclosure } from '@chakra-ui/react';
+import {
+  CommonButton,
+  CommonDrawer,
+  CommonRadio,
+  CommonText,
+  CommonTextarea,
+  Header,
+} from '@/shared/components';
 import { Container, ContentsWrapper, ContentsBox } from './style';
 
 const initalHobby = ['수영', '자전거', '농구'];
@@ -10,15 +18,16 @@ interface Textarea {
 }
 
 const FeedCreate = () => {
-  const [selected, setSelected] = useState(initalHobby[0]);
+  const [selectedHobby, setSelectedHobby] = useState(initalHobby[0]);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<Textarea>();
-  const onSubmit: SubmitHandler<Textarea> = (data) => console.log(data);
-
-  console.log(selected);
+  const onSubmit: SubmitHandler<Textarea> = (data) => {
+    console.log(data, selectedHobby);
+  };
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
@@ -28,11 +37,15 @@ const FeedCreate = () => {
           <CommonText type="normalTitle">피드 생성하기</CommonText>
           <ContentsBox>
             <CommonText type="normalInfo">취미를 선택해주세요.</CommonText>
-            <CommonRadio values={initalHobby} name="취미" onChange={setSelected} />
+            <CommonRadio
+              values={initalHobby}
+              name="취미"
+              onChange={(value: string) => setSelectedHobby(value)}
+            />
           </ContentsBox>
           <ContentsBox>
             <CommonText type="normalInfo">버킷을 선택해주세요.</CommonText>
-            <CommonButton type="custom" isDisabled={false} onClick={() => {}} />
+            <CommonButton type="custom" onClick={onOpen} />
           </ContentsBox>
           <ContentsBox>
             <CommonText type="normalInfo">피드 내용을 입력해주세요.</CommonText>
@@ -41,7 +54,9 @@ const FeedCreate = () => {
                 size="sm"
                 placeholder="내용을 입력해주세요."
                 error={errors.textarea}
-                {...register('textarea')}
+                {...register('textarea', {
+                  required: '내용을 필수로 입력해주세요.',
+                })}
               />
             </form>
           </ContentsBox>
@@ -50,6 +65,15 @@ const FeedCreate = () => {
         <CommonButton type="mdMiddle" isDisabled={false} onClick={() => {}}>
           생성 완료
         </CommonButton>
+        <CommonDrawer
+          isOpen={isOpen}
+          onClose={onClose}
+          onClickFooterButton={() => {}}
+          isFull={true}
+          footerButtonText="선택 완료"
+        >
+          1
+        </CommonDrawer>
       </Container>
     </>
   );
