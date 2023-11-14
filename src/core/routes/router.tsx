3c7d0db960e174fb491e04e9e829084606388484
chan uk jeong <40534414+seeyoujeong@users.noreply.github.com>
2023-11-14 15:16:43 +0900

    fix: import 중복 제거

--- src/core/routes/router.tsx
+++ src/core/routes/router.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import { FeedCreate, FeedDetail } from '@/pages';
 import ItemList from '@/pages/Item/List';
-import { FeedCreate } from '@/pages';
 import VoteCreate from '@/pages/Vote/VoteCreate';
 import VoteDetail from '@/pages/Vote/VoteDetail';
 import VoteHome from '@/pages/Vote/VoteHome';
