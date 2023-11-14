9ccf4f1927230b7d39b2aab1adc90ca39be2b635
seeyoujeong <chan3358@naver.com>
2023-11-14 21:47:35 +0900

    feat: Votes 컴포넌트에 useSearchParams 추가

--- src/features/vote/components/Votes/index.tsx
+++ src/features/vote/components/Votes/index.tsx
@@ -1,16 +1,28 @@
+import { useSearchParams } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { CommonTabs } from '@/shared/components';
 import VoteItem from '../VoteItem';
 
+const voteState = ['ended', 'uploaded', 'participated'];
+
 const Votes = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   return (
     <Container>
       <CommonTabs
+        currentTabIndex={voteState.indexOf(searchParams.get('state') || voteState[0])}
+        onClick={(value) => {
+          searchParams.set('state', value);
+          setSearchParams(searchParams);
+        }}
         tabsData={[
           {
+            value: 'ended',
             label: '종료된 투표',
             content: (
               <ContentsWrapper>
+                <div>1</div>
                 <VoteItem />
                 <VoteItem />
                 <VoteItem />
@@ -19,12 +31,30 @@ const Votes = () => {
             ),
           },
           {
+            value: 'uploaded',
             label: '올린 투표',
-            content: '',
+            content: (
+              <ContentsWrapper>
+                <div>2</div>
+                <VoteItem />
+                <VoteItem />
+                <VoteItem />
+                <VoteItem />
+              </ContentsWrapper>
+            ),
           },
           {
+            value: 'participated',
             label: '참여한 투표',
-            content: '',
+            content: (
+              <ContentsWrapper>
+                <div>3</div>
+                <VoteItem />
+                <VoteItem />
+                <VoteItem />
+                <VoteItem />
+              </ContentsWrapper>
+            ),
           },
         ]}
       />
@@ -46,6 +76,6 @@ const ContentsWrapper = styled.div`
   align-items: center;
   gap: 1rem;
   overflow-y: scroll;
-  height: calc(100vh - 20rem);
+  height: calc(100vh - 30.5rem);
   padding: 1rem 0;
 `;
