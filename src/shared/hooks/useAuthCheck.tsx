23dbe2e2a5db184c866670a0da216d0b00640e44
kim <perfumeplaylist.gmail.com>
2023-11-15 02:39:48 +0900

    feat: useAuthCheck 기능 구현

---
+++ src/shared/hooks/useAuthCheck.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import { Storage } from '@/shared/utils';
+
+const useAuthCheck = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  useEffect(() => {
+    const token = Storage.getLocalStoraged('token');
+    if (token !== 'null') {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  }, []);
+
+  return isLogin;
+};
+
+export default useAuthCheck;
