8f5bb77d52bf3d79bd89ba32e148cd32b9014f77
seeyoujeong <chan3358@naver.com>
2023-11-14 21:46:41 +0900

    refactor: CommonTabs 컴포넌트 수정

--- src/shared/components/Tabs/index.tsx
+++ src/shared/components/Tabs/index.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 import { Tabs, TabList, TabPanels, Tab, TabPanel, TabsProps } from '@chakra-ui/react';
 
 interface TabsData {
+  value?: string;
   label: string;
   content: string | ReactElement;
 }
@@ -9,28 +10,39 @@ interface CommonTabsProps {
   tabsType?: 'soft-rounded' | 'line';
   isFitted?: TabsProps['isFitted'];
   tabsData: TabsData[];
-  onClick?: () => void;
+  onClick?: (value: string) => void;
+  currentTabIndex?: number;
 }
 
-const CommonTabs = ({ tabsData, isFitted = true, tabsType = 'line', onClick }: CommonTabsProps) => {
-  const handleClick = () => {
-    onClick && onClick();
+const selectedStyle = (tabsType: string) => {
+  return tabsType === 'line'
+    ? { color: 'blue.300', borderColor: 'blue.300' }
+    : { color: 'white', bg: 'blue.300' };
+};
+
+const CommonTabs = ({
+  tabsData,
+  isFitted = true,
+  tabsType = 'line',
+  onClick,
+  currentTabIndex = 0,
+}: CommonTabsProps) => {
+  const handleClick = (value: string) => {
+    onClick && onClick(value);
   };
 
   return (
-    <Tabs isFitted={isFitted} variant={tabsType} size="sm">
+    <Tabs defaultIndex={currentTabIndex} isFitted={isFitted} variant={tabsType} size="sm">
       <TabList padding={tabsType === 'soft-rounded' ? '1rem 0 0 1rem' : undefined}>
         {tabsData.map((tab, index) => (
           <Tab
-            onClick={handleClick}
+            onClick={() => handleClick(tab.value ?? '')}
             color="blue.900"
             bg="none"
-            _selected={
-              tabsType === 'line'
-                ? { color: 'blue.300', borderColor: 'blue.300' }
-                : { color: 'white', bg: 'blue.300' }
-            }
+            _selected={selectedStyle(tabsType)}
             key={index}
+            _disabled={selectedStyle(tabsType)}
+            isDisabled={currentTabIndex === index}
           >
             {tab.label}
           </Tab>
