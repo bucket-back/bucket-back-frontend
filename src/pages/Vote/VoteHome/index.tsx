8e9a5d096e0dbd6bb8ccd09d550e6276a19243ec
seeyoujeong <chan3358@naver.com>
2023-11-14 21:48:05 +0900

    feat: VoteHome에 useSearchParams 추가

--- src/pages/Vote/VoteHome/index.tsx
+++ src/pages/Vote/VoteHome/index.tsx
@@ -1,16 +1,27 @@
+import { useSearchParams } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { CommonTabs } from '@/shared/components';
 import VoteInProgress from '@/features/vote/components/VoteInProgress';
 import Votes from '@/features/vote/components/Votes';
 
+const hobby = ['cycle', 'swim', 'basketball'];
+
 const VoteHome = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   return (
     <Container>
       <CommonTabs
+        currentTabIndex={hobby.indexOf(searchParams.get('hobby') || hobby[0])}
         tabsType="soft-rounded"
         isFitted={false}
+        onClick={(value) => {
+          searchParams.set('hobby', value);
+          setSearchParams(searchParams);
+        }}
         tabsData={[
           {
+            value: 'cycle',
             label: '자전거',
             content: (
               <>
@@ -20,12 +31,24 @@ const VoteHome = () => {
             ),
           },
           {
+            value: 'swim',
             label: '수영',
-            content: '',
+            content: (
+              <>
+                <VoteInProgress />
+                <Votes />
+              </>
+            ),
           },
           {
+            value: 'basketball',
             label: '농구',
-            content: '',
+            content: (
+              <>
+                <VoteInProgress />
+                <Votes />
+              </>
+            ),
           },
         ]}
       />
