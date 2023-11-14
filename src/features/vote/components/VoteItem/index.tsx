f3936f6959fecb5908137f25d6a919d3f4fef6d9
seeyoujeong <chan3358@naver.com>
2023-11-14 14:43:20 +0900

    design: VoteItem 컴포넌트 수정

--- src/features/vote/components/VoteItem/index.tsx
+++ src/features/vote/components/VoteItem/index.tsx
@@ -1,27 +1,49 @@
-import { Box, Flex } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import { CommonCard, CommonImage, CommonText } from '@/shared/components';
 
 const VoteItem = () => {
   return (
     <CommonCard count={0} date="2023-11-12T12:16:49.407Z" onClick={() => {}}>
-      <Flex flexDir="column" gap="1rem">
+      <ContentsContainer>
         <CommonText type="smallInfo">이거 엄마한테 선물드릴려고하는데 어떰?</CommonText>
-        <Flex alignItems="center" gap="0.5rem">
-          <Box>
+        <ContentsWrapper>
+          <ContentsBox>
             <CommonImage size="base" />
             <CommonText type="smallInfo">29,800</CommonText>
             <CommonText type="smallInfo">아이템 이름입니다.</CommonText>
-          </Box>
-          <CommonText type="smallInfo">VS</CommonText>
-          <Box>
+          </ContentsBox>
+          <VsBox>
+            <CommonText type="smallInfo" noOfLines={0}>
+              VS
+            </CommonText>
+          </VsBox>
+          <ContentsBox>
             <CommonImage size="base" />
             <CommonText type="smallInfo">29,800</CommonText>
             <CommonText type="smallInfo">아이템 이름입니다.</CommonText>
-          </Box>
-        </Flex>
-      </Flex>
+          </ContentsBox>
+        </ContentsWrapper>
+      </ContentsContainer>
     </CommonCard>
   );
 };
 
 export default VoteItem;
+
+const ContentsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const ContentsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const ContentsBox = styled.div``;
+
+const VsBox = styled.div`
+  flex-shrink: 0;
+`;
