d7bea24453e9e2887ee789008eb56ad294d676fd
seeyoujeong <chan3358@naver.com>
2023-11-14 14:42:54 +0900

    design: VoteInProgress 컴포넌트 수정

--- src/features/vote/components/VoteInProgress/index.tsx
+++ src/features/vote/components/VoteInProgress/index.tsx
@@ -1,30 +1,53 @@
-import { Flex } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import { CommonText, DividerImage } from '@/shared/components';
 
 const TEMP_IMAGE = 'https://placehold.co/800?text=Bucket+Back&font=roboto';
 
 const VoteInProgress = () => {
   return (
-    <Flex flexDir="column" gap="1rem">
-      <CommonText type="normalInfo">진행중인 투표</CommonText>
-      <Flex overflowX="auto" gap="0.7rem">
+    <Container>
+      <TitleWrapper>
+        <CommonText type="normalInfo">진행중인 투표</CommonText>
+      </TitleWrapper>
+      <ContentsWrapper>
         {Array.from({ length: 10 }, (_, index) => {
           return (
-            <Flex
-              key={index}
-              flexDir="column"
-              gap="0.5rem"
-              alignItems="center"
-              marginBottom="1.5rem"
-            >
+            <ContentsBox key={index}>
               <DividerImage type="live" images={[TEMP_IMAGE, TEMP_IMAGE]} />
               <CommonText type="smallInfo">00명 참여중!</CommonText>
-            </Flex>
+            </ContentsBox>
           );
         })}
-      </Flex>
-    </Flex>
+      </ContentsWrapper>
+    </Container>
   );
 };
 
 export default VoteInProgress;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const TitleWrapper = styled.div`
+  padding-top: 1rem;
+  padding-left: 1.25rem;
+`;
+
+const ContentsWrapper = styled.div`
+  display: flex;
+  gap: 0.7rem;
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+  padding-bottom: 1rem;
+  overflow-x: scroll;
+`;
+
+const ContentsBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+`;
