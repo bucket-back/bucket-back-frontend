f0e4841cad8a35522b8ed1c9e0fc1568a1c1cca7
seeyoujeong <chan3358@naver.com>
2023-11-14 14:43:51 +0900

    design: Vote 컴포넌트 수정

--- src/features/vote/components/Votes/index.tsx
+++ src/features/vote/components/Votes/index.tsx
@@ -1,21 +1,21 @@
-import { Flex } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import { CommonTabs } from '@/shared/components';
 import VoteItem from '../VoteItem';
 
 const Votes = () => {
   return (
-    <>
+    <Container>
       <CommonTabs
         tabsData={[
           {
             label: '종료된 투표',
             content: (
-              <Flex flexDir="column" alignItems="center" gap="2rem" overflowY="scroll">
+              <ContentsWrapper>
                 <VoteItem />
                 <VoteItem />
                 <VoteItem />
                 <VoteItem />
-              </Flex>
+              </ContentsWrapper>
             ),
           },
           {
@@ -28,8 +28,24 @@ const Votes = () => {
           },
         ]}
       />
-    </>
+    </Container>
   );
 };
 
 export default Votes;
+
+const Container = styled.div`
+  background-color: white;
+  padding-top: 2rem;
+  border-radius: 2rem 2rem 0 0;
+`;
+
+const ContentsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  overflow-y: scroll;
+  height: calc(100vh - 20rem);
+  padding: 1rem 0;
+`;
