4544fd6a9b89fe437ad358e89c22c3adcc63d62d
daeun <gbajs754@gmail.com>
2023-11-15 22:47:14 +0900

    fix: 파일명 수정

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { CommonButton, CommonIcon, CommonInput } from '@/shared/components';
 import { ButtonWrapper, Container, Form, IconWrapper, InputWrapper } from './style';
-import { useLogin } from '@/features/SingIn/service/login';
+import { useLogin } from '@/features/Singin/service/login';
 import useValidateForm from '@/shared/hooks/useValidateForm';
 
 interface Login {
