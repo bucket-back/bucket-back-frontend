13e9afa71fe38a4e08ea217c7f6f426a905851fd
seeyoujeong <chan3358@naver.com>
2023-11-16 15:27:34 +0900

    fix: import를 최상단으로 이동

--- src/pages/Member/MemberHome/style.tsx
+++ src/pages/Member/MemberHome/style.tsx
@@ -1,7 +1,8 @@
+import styled from '@emotion/styled';
+
 export const Container = styled.div`
   height: 100%;
 `;
-import styled from '@emotion/styled';
 
 export const MemberInfoWrapper = styled.div`
   display: flex;
