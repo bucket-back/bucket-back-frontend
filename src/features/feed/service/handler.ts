fa68cae0666b90286897ac473eccd050c49b2dab
seeyoujeong <chan3358@naver.com>
2023-11-17 15:36:35 +0900

    refactor: 타입 분리

--- src/features/feed/service/handler.ts
+++ src/features/feed/service/handler.ts
@@ -1,106 +1,33 @@
+import {
+  GetFeedDetailResponse,
+  GetFeedsRequest,
+  GetFeedsResponse,
+  PostFeedRequest,
+  PostFeedResponse,
+} from './types';
 import { axiosClient } from '@/core/service/axios';
 
-const FEEDS = 'feeds';
+const BASE_URL = 'feeds';
 
-interface FeedItems {
-  id: number;
-  image: string;
-  url: string;
-}
+export const feedApi = {
+  getFeeds: async ({ hobbyName, nickname, sortCondition, cursorId, size }: GetFeedsRequest) => {
+    const res = await axiosClient.get<GetFeedsResponse>(
+      `${BASE_URL}?hobbyName=${hobbyName}${nickname ? `&nickname=${nickname}` : ''}${
+        sortCondition ? `&sortCondition=${sortCondition}` : ''
+      }`,
+      { params: cursorId ? { cursorId, size } : { size } }
+    );
 
-interface FeedsData {
-  cursorId: string;
-  memberInfo: MemberInfo;
-  feedId: number;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-  createdAt: string;
-  feedItems: FeedItems[];
-  isLike: boolean;
-}
+    return res.data;
+  },
+  getFeedDetail: async (feedId: number) => {
+    const res = await axiosClient.get<GetFeedDetailResponse>(`${BASE_URL}/${feedId}`);
 
-interface Feeds {
-  nextCursorId: string;
-  feeds: FeedsData[];
-}
+    return res.data;
+  },
+  postFeed: async ({ bucketId, content }: PostFeedRequest) => {
+    const res = await axiosClient.post<PostFeedResponse>(BASE_URL, { bucketId, content });
 
-interface GetFeedsProps {
-  hobbyName: string;
-  nickname?: string;
-  sortCondition?: 'popularity' | 'latest';
-  cursorId?: string;
-  size: number;
-}
-
-export const getFeeds = async ({
-  hobbyName,
-  nickname,
-  sortCondition,
-  cursorId,
-  size,
-}: GetFeedsProps) => {
-  const res = await axiosClient.get<Feeds>(
-    `${FEEDS}?hobbyName=${hobbyName}${nickname ? `&nickname=${nickname}` : ''}${
-      sortCondition ? `&sortCondition=${sortCondition}` : ''
-    }`,
-    { params: cursorId ? { cursorId, size } : { size } }
-  );
-
-  return res.data;
-};
-
-interface MemberInfo {
-  memberId: number;
-  nickName: string;
-  profileImage: string;
-  level: number;
-}
-
-interface FeedInfo {
-  id: number;
-  hobby: string;
-  content: {
-    content: string;
-  };
-  bucketName: string;
-  bucketBudget: number;
-  createdAt: string;
-  hasAdoptedComment: boolean;
-  likeCount: number;
-  isLiked: boolean;
-}
-
-interface FeedItem {
-  id: number;
-  name: string;
-  price: number;
-  image: string;
-}
-
-interface FeedDetail {
-  memberInfo: MemberInfo;
-  feedInfo: FeedInfo;
-  feedItems: FeedItem[];
-}
-
-export const getFeedDetail = async (feedId: number) => {
-  const res = await axiosClient.get<FeedDetail>(`${FEEDS}/${feedId}`);
-
-  return res.data;
-};
-
-interface PostFeedRes {
-  feedId: number;
-}
-
-interface PostFeedProps {
-  bucketId: number;
-  content: string;
-}
-
-export const postFeed = async ({ bucketId, content }: PostFeedProps) => {
-  const res = await axiosClient.post<PostFeedRes>(FEEDS, { bucketId, content });
-
-  return res.data;
+    return res.data;
+  },
 };
--- src/shared/types/feed.ts
+++ src/shared/types/feed.ts
@@ -1,16 +1,7 @@
-export interface DetailFeedRes {
-  memberInfo: MemberInfo;
-  feedInfo: FeedInfo;
-  feedItems: FeedItem[];
-}
-export interface SearchFeedCommentRes {
-  commentCount: number;
-  comments: Comment[];
-}
-
-export interface SearchFeedRes {
-  nextCursorId: string;
-  voteSummaries: VoteSummary[];
+export interface FeedItems {
+  id: number;
+  image: string;
+  url: string;
 }
 
 export interface MemberInfo {
@@ -20,57 +11,31 @@ export interface MemberInfo {
   level: number;
 }
 
-export interface FeedInfo {
-  hobby: string;
+export interface FeedsData {
+  cursorId: string;
+  memberInfo: MemberInfo;
   feedId: number;
+  content: string;
   likeCount: number;
-  feedContent: string;
+  commentCount: number;
+  createdAt: string;
+  feedItems: FeedItems[];
+  isLike: boolean;
+}
+
+export interface FeedInfo {
+  id: number;
+  hobby: string;
+  content: string;
   bucketName: string;
   bucketBudget: number;
-  isLike: boolean;
-  hasAdoptedComment: boolean;
   createdAt: string;
+  hasAdoptedComment: boolean;
+  likeCount: number;
+  isLiked: boolean;
 }
 
 export interface FeedItem {
-  itemId: number;
-  itemName: string;
-  itemPrice: number;
-  itemImage: string;
-}
-
-export interface Comment {
-  memberInfo: MemberInfo;
-  comment: string;
-  likeCount: number;
-  isLike: boolean;
-  isAdopted: boolean;
-  createAt: string;
-}
-
-export interface VoteSummary {
-  voteInfo: VoteInfo;
-  option1Item: Option1Item;
-  option2Item: Option2Item;
-  cursorId: string;
-}
-
-export interface VoteInfo {
-  id: number;
-  content: string;
-  startTime: string;
-  isVoting: boolean;
-  participants: number;
-}
-
-export interface Option1Item {
-  id: number;
-  name: string;
-  price: number;
-  image: string;
-}
-
-export interface Option2Item {
   id: number;
   name: string;
   price: number;
