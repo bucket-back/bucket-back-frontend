07da9bc120960551fd6198aada876953fe5c92fc
seeyoujeong <chan3358@naver.com>
2023-11-19 00:14:11 +0900

    feat: 프로필 수정 페이지 UI 구현

---
+++ src/pages/Member/MemberEdit/style.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  height: 100%;
+  padding: 0 3.5rem;
+`;
+
+export const Form = styled.form`
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 0.8rem;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const AvatarBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const InputBox = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 1rem;
+`;
