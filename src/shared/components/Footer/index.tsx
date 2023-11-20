19e5edc02a615d0f5929e1eba7b5a5706aab714c
seeyoujeong <chan3358@naver.com>
2023-11-20 18:08:51 +0900

    feat: 푸터의 navigate 수정

--- src/shared/components/Footer/index.tsx
+++ src/shared/components/Footer/index.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Flex } from '@chakra-ui/react';
 import { CommonIcon, CommonText } from '@/shared/components';
+import { useMovePageWithLogin, useUserInfo } from '@/shared/hooks';
 import useSamePath from '@/shared/hooks/useSamePath';
 import { COMMON } from '@/shared/styles/Common';
 
@@ -37,11 +38,13 @@ type FooterIcon = 'home' | 'search' | 'bucket' | 'item' | 'user';
 const Footer = ({ children }: FooterProps) => {
   const navigate = useNavigate();
   const isSamePath = useSamePath();
+  const userInfo = useUserInfo();
+  const movePage = useMovePageWithLogin();
   const handleClick = (type: string) => {
     if (isSamePath(type)) {
       return;
     }
-    // 추후 페이지 이동 + 아이콘 클릭하면 페이지 상단으로 올라갈수있도록 로직 추가
+
     switch (type) {
       case 'home': {
         navigate('/');
@@ -52,15 +55,15 @@ const Footer = ({ children }: FooterProps) => {
         break;
       }
       case 'bucket': {
-        navigate('/bucket/create');
+        movePage('/bucket/create');
         break;
       }
       case 'item': {
-        navigate('/item');
+        movePage('/item');
         break;
       }
       case 'user': {
-        // navigate(`/user/${:userId}`);
+        movePage(`/member/${userInfo?.nickname}`);
         break;
       }
     }
