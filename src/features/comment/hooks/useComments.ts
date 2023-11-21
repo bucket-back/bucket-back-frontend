9e494f3454c6b5a15cb75ac33f1d2811b0d8d4d6
seeyoujeong <chan3358@naver.com>
2023-11-21 19:52:14 +0900

    refactor: FeedComment 컴포넌트를 CommentItem으로 수정

--- src/features/comment/hooks/useComments.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { GetCommentsRequest, commentQueryQption } from '../service';
-
-const useComments = ({ feedId, cursorId, size }: GetCommentsRequest) => {
-  return useQuery(commentQueryQption.list({ feedId, cursorId, size }));
-};
-
-export default useComments;
