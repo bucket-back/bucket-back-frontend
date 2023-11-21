b381f215383280ad57b1d86883084861e796331e
seeyoujeong <chan3358@naver.com>
2023-11-22 00:52:11 +0900

    feat: 비로그인시 토스트 추가

--- src/features/feed/hooks/useFeedLike.ts
+++ src/features/feed/hooks/useFeedLike.ts
@@ -1,13 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useCustomToast } from '@/shared/hooks';
 import { feedApi, feedQueryOption } from '../service';
 
 const useFeedLike = () => {
   const queryClient = useQueryClient();
+  const openToast = useCustomToast();
 
   return useMutation({
     mutationFn: feedApi.postFeedLike,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: feedQueryOption.all });
+    },
+    onError: () => {
+      openToast({ message: '로그인후 이용가능합니다.', type: 'info' });
     },
   });
 };
