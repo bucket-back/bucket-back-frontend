2f2d4e692890d36c8029611775f435d48b98270b
seeyoujeong <chan3358@naver.com>
2023-11-21 14:35:37 +0900

    fix: queryClient 밖으로 빼기

--- src/core/query/QueryClientProvider.tsx
+++ src/core/query/QueryClientProvider.tsx
@@ -2,21 +2,20 @@ import { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider as BaseQueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      refetchOnWindowFocus: false,
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
+
 const QueryClientProvider = (props: PropsWithChildren<unknown>) => {
   const { children } = props;
-
-  // retry가 기본 3번이기 때문에 줄이기 위해서 0으로 설정!
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: 0,
-        refetchOnWindowFocus: false,
-      },
-      mutations: {
-        retry: 0,
-      },
-    },
-  });
 
   return (
     <BaseQueryClientProvider client={queryClient}>
