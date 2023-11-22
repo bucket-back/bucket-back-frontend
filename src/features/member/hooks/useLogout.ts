9f762287c90fd4a45abb99202a642d044414a98f
seeyoujeong <chan3358@naver.com>
2023-11-22 15:36:35 +0900

    refactor: 상수 사용

--- src/features/member/hooks/useLogout.ts
+++ src/features/member/hooks/useLogout.ts
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { TOKEN_KEY, USER_INFO_KEY } from '@/shared/constants';
+import { ROOT_PATH, TOKEN_KEY, USER_INFO_KEY } from '@/shared/constants';
 import { useCustomToast } from '@/shared/hooks';
 import { Storage } from '@/shared/utils';
 
@@ -11,7 +11,7 @@ const useLogout = () => {
     Storage.removeLocalStoraged(USER_INFO_KEY);
     Storage.removeLocalStoraged(TOKEN_KEY);
     openToast({ message: '로그아웃 되었습니다.', type: 'success' });
-    navigate('/');
+    navigate(ROOT_PATH);
   };
 };
 
