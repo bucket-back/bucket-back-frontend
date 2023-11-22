23254a56d7cb77dc9b80216cdd5677927b50f833
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-11-22 14:13:50 +0900

    [OMCT-227] 아이템 생성 api 연결 (#115)
    * feat: item handler 설정
    
    * feat: item handler 구현
    
    * feat: queryOption 작성
    
    * feat: 취미 불러오는 api 적용
    
    * fix: 아이템 생성 api 수정
    
    * refactor: usePostItem 훅으로 분리/ axios response 변경
    
    * refactor: mutate 이름 수정 및 타입 수정
    
    * fix: authNavigate 훅 추가
    
    ---------
    
    Co-authored-by: kim <perfumeplaylist.gmail.com>

---
+++ src/features/item/hooks/index.ts
@@ -0,0 +1,1 @@
+export { default as usePostItem } from './usePostItem';
---
+++ src/features/item/service/queryOption.ts
@@ -0,0 +1,13 @@
+import { queryOptions } from '@tanstack/react-query';
+import { GetSearchItemRequest, itemApi } from '.';
+
+const itemQueryOption = {
+  all: ['item'] as const,
+  list: ({ keyword, cursorId, size }: GetSearchItemRequest) =>
+    queryOptions({
+      queryKey: [...itemQueryOption.all, keyword] as const,
+      queryFn: () => itemApi.getSearchItem({ keyword, cursorId, size }),
+    }),
+};
+
+export default itemQueryOption;
