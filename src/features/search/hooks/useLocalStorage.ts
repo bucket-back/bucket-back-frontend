2f534d97bd558af5c0d8fc983f4fc9cfaadcd780
kim <perfumeplaylist.gmail.com>
2023-11-24 16:30:16 +0900

    fix: localStorage 훅 타입 수정

--- src/features/search/hooks/useLocalStorage.ts
+++ src/features/search/hooks/useLocalStorage.ts
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { Storage } from '@/shared/utils';
 
-const useLocalStorage = (key: string): [string[], (tempValue: string[]) => void] => {
-  const [storageValue, setStorageValue] = useState<string[]>(Storage.getLocalStoraged(key));
+const useLocalStorage = (key: string): [string[] | [], (tempValue: string[] | []) => void] => {
+  const [storageValue, setStorageValue] = useState<string[] | []>(Storage.getLocalStoraged(key));
 
-  const setState = (tempValue: string[]) => {
+  const setState = (tempValue: string[] | []) => {
     setStorageValue(tempValue);
-    Storage.setLocalStoraged<string[]>(key, tempValue);
+    Storage.setLocalStoraged<string[] | []>(key, tempValue);
   };
 
   return [storageValue, setState];
