de953fd51ecee7971664d8a3abf9c07ca127da92
kim <perfumeplaylist.gmail.com>
2023-11-24 15:48:39 +0900

    feat: 로컬스토리지 훅 구현

---
+++ src/features/search/hooks/useLocalStorage.ts
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { Storage } from '@/shared/utils';
+
+const useLocalStorage = (key: string): [string[], (tempValue: string[]) => void] => {
+  const [storageValue, setStorageValue] = useState<string[]>(Storage.getLocalStoraged(key));
+
+  const setState = (tempValue: string[]) => {
+    setStorageValue(tempValue);
+    Storage.setLocalStoraged<string[]>(key, tempValue);
+  };
+
+  return [storageValue, setState];
+};
+
+export default useLocalStorage;
