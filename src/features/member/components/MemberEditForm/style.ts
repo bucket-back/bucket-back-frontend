687c69d2262bd1ab75d804145640af761763c4b5
seeyoujeong <chan3358@naver.com>
2023-11-24 18:11:10 +0900

    feat: MemberEditForm 컴포넌트 추가

---
+++ src/features/member/components/MemberEditForm/style.ts
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 0.8rem;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const AvatarBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const InputBox = styled.div`
+  display: flex;
+  align-items: start;
+  gap: 0.5rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 1rem;
+`;
