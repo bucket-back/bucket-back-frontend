dc14e8f46216de974e25d32e6c55d8b4913f2370
chan uk jeong <40534414+seeyoujeong@users.noreply.github.com>
2023-11-25 01:47:45 +0900

    [OMCT-326] 비밀번호 변경 기능 연결 (#153)
    * feat: useUpdatePassword 훅 추가
    
    * feat: 컴포넌트 분리 및 비밀번호 변경 기능 추가
    
    * feat: 토큰 검사 추가
    
    * fix: 오타 수정

--- src/pages/Member/MemberPassword/style.tsx
+++ src/pages/Member/MemberPassword/style.tsx
@@ -4,20 +4,3 @@ export const Container = styled.div`
   height: 100%;
   padding: 0 3.5rem;
 `;
-
-export const FormWrapper = styled.form`
-  height: 90%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-export const InputPanel = styled.div`
-  height: 50%;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 1rem;
-`;
