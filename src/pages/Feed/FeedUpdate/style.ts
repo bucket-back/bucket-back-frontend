160a28b63a4cc65bc6a2b94bd2c0aaf62eeeac6b
seeyoujeong <chan3358@naver.com>
2023-11-25 01:14:02 +0900

    feat: 피드 수정 페이지 추가

---
+++ src/pages/Feed/FeedUpdate/style.ts
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 2.44rem 2.44rem 2.44rem;
+  gap: 1.5rem;
+`;
+
+export const Form = styled.form`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const ContentsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const ContentsPanel = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+export const SelectedBucketBox = styled.div`
+  width: 6.125rem;
+  height: 5.625rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
