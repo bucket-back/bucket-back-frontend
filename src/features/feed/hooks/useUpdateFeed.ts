463f67d575f424159b3cd2ae620df8e95062dd18
seeyoujeong <chan3358@naver.com>
2023-11-25 13:56:38 +0900

    feat: 에러시 토스트나오게 수정

--- src/features/feed/hooks/useUpdateFeed.ts
+++ src/features/feed/hooks/useUpdateFeed.ts
@@ -13,6 +13,9 @@ const useUpdateFeed = (feedId: string) => {
       openToast({ message: '피드가 수정되었습니다.', type: 'success' });
       navigate(`/feed/${feedId}`);
     },
+    onError: () => {
+      openToast({ message: '피드 수정에 실패했습니다.', type: 'error' });
+    },
   });
 };
 
