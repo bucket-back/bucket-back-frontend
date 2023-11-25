26b56ae36d19f3a8ffd76aa6c63a642a387f87f6
chan uk jeong <40534414+seeyoujeong@users.noreply.github.com>
2023-11-25 16:45:32 +0900

    [OMCT-337] 피드 수정 api 연결 (#156)
    * feat: useUpdateFeed 훅 추가
    
    * feat: 피드 내용에 useForm 연결
    
    * fix: 메서드 오타 수정
    
    * feat: 에러시 토스트나오게 수정
    
    * feat: 피드 수정 api 연결

--- src/pages/Feed/FeedUpdate/index.tsx
+++ src/pages/Feed/FeedUpdate/index.tsx
@@ -1,3 +1,6 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 import {
   CommonButton,
   CommonText,
@@ -13,29 +16,73 @@ import {
   Form,
   SelectedBucketBox,
 } from './style';
+import { useUpdateFeed } from '@/features/feed/hooks';
+import { feedQueryOption } from '@/features/feed/service';
 import { HobbyRadio } from '@/features/hobby/components';
 
+interface FeedContent {
+  content: string;
+}
+
 const FeedUpdate = () => {
+  const { feedId } = useParams();
+  const feedDetail = useQuery({
+    ...feedQueryOption.detail(Number(feedId)),
+    select: (data) => {
+      const hobby = data.feedInfo.hobby;
+      const images = data.feedItems.map(({ image }) => image);
+      const content = data.feedInfo.content;
+
+      return { hobby, images, content };
+    },
+  });
+  const updateFeed = useUpdateFeed(feedId!);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FeedContent>({
+    mode: 'onBlur',
+    values: { content: feedDetail.data?.content || '' },
+  });
+
+  const onSubmit: SubmitHandler<FeedContent> = ({ content }) => {
+    updateFeed.mutate({ feedId: Number(feedId), content });
+  };
+
+  if (feedDetail.isPending) {
+    return;
+  }
+
+  if (feedDetail.isError) {
+    return;
+  }
+
   return (
     <>
       <Header type="back" />
       <Container>
         <CommonText type="normalTitle">피드 수정하기</CommonText>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <ContentsWrapper>
             <ContentsPanel>
-              <CommonText type="normalInfo">취미를 선택해주세요.</CommonText>
+              <CommonText type="normalInfo">선택한 취미입니다.</CommonText>
               <HobbyRadio defaultValue="야구" isReadOnly />
             </ContentsPanel>
             <ContentsPanel>
-              <CommonText type="normalInfo">버킷을 선택해주세요.</CommonText>
+              <CommonText type="normalInfo">선택한 버킷입니다.</CommonText>
               <SelectedBucketBox>
-                <DividerImage images={['1', '2', '3']} type="base" />
+                <DividerImage images={feedDetail.data.images} type="base" />
               </SelectedBucketBox>
             </ContentsPanel>
             <ContentsPanel>
               <CommonText type="normalInfo">피드 내용을 입력해주세요.</CommonText>
-              <CommonTextarea size="sm" placeholder="내용을 입력해주세요." />
+              <CommonTextarea
+                size="sm"
+                placeholder="내용을 입력해주세요."
+                error={errors.content}
+                {...register('content', { required: '내용을 필수로 입력해주세요.' })}
+              />
             </ContentsPanel>
           </ContentsWrapper>
           <ButtonWrapper>
