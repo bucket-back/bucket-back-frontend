9a717228710a0a579b91ec79216ef8a0bbca602d
seeyoujeong <chan3358@naver.com>
2023-11-25 15:02:04 +0900

    feat: 댓글 채택하기 api 연결

--- src/features/comment/hooks/index.ts
+++ src/features/comment/hooks/index.ts
@@ -1,2 +1,4 @@
 export { default as useAddComment } from './useAddComment';
 export { default as useDeleteComment } from './useDeleteComment';
+export { default as useAdoptComment } from './useAdoptComment';
+export { default as useUpdateComment } from './useUpdateComment';
