8b56fe97479e713d659e39e804b4769c4e37b442
seeyoujeong <chan3358@naver.com>
2023-11-25 23:14:08 +0900

    feat: queryKey에 as const 추가

--- src/features/bucket/service/queryOption.ts
+++ src/features/bucket/service/queryOption.ts
@@ -6,19 +6,19 @@ const QUERY_KEY = 'bucket';
 const bucketQueryOption = {
   list: ({ nickname, hobby, cursorId, size = 10 }: GetBucketsRequest) =>
     queryOptions({
-      queryKey: [QUERY_KEY, nickname, hobby],
+      queryKey: [QUERY_KEY, nickname, hobby] as const,
       queryFn: () => bucketApi.getBuckets({ nickname, hobby, cursorId, size }),
     }),
 
   detail: ({ nickname, bucketId }: GetBucketDetailRequest) =>
     queryOptions({
-      queryKey: [QUERY_KEY, nickname, bucketId],
+      queryKey: [QUERY_KEY, nickname, bucketId] as const,
       queryFn: () => bucketApi.getBucketDetail({ nickname, bucketId }),
     }),
 
   myItemList: ({ bucketId, cursorId, size }: GetBucketMyItemsRequest) =>
     queryOptions({
-      queryKey: [QUERY_KEY, bucketId],
+      queryKey: [QUERY_KEY, bucketId] as const,
       queryFn: () => bucketApi.getBucketMyItems({ bucketId, cursorId, size }),
     }),
 };
