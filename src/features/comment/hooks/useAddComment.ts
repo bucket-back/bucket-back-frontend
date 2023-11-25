6a378831b0f9a6bb78953e73d2682c118f2bf71a
seeyoujeong <chan3358@naver.com>
2023-11-25 15:45:37 +0900

    feat: 성공시 토스트 추가

--- src/features/comment/hooks/useAddComment.ts
+++ src/features/comment/hooks/useAddComment.ts
@@ -1,13 +1,16 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useCustomToast } from '@/shared/hooks';
 import { commentApi, commentQueryQption } from '../service';
 
 const useAddComment = () => {
   const queryClient = useQueryClient();
+  const openToast = useCustomToast();
 
   return useMutation({
     mutationFn: commentApi.postComment,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: commentQueryQption.all });
+      openToast({ message: '댓글이 추가되었습니다.', type: 'success' });
     },
   });
 };
--- src/features/comment/hooks/useUpdateComment.ts
+++ src/features/comment/hooks/useUpdateComment.ts
@@ -1,13 +1,16 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useCustomToast } from '@/shared/hooks';
 import { commentApi, commentQueryQption } from '../service';
 
 const useUpdateComment = () => {
   const queryClient = useQueryClient();
+  const openToast = useCustomToast();
 
   return useMutation({
     mutationFn: commentApi.putComment,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: commentQueryQption.all });
+      openToast({ message: '댓글이 수정되었습니다.', type: 'success' });
     },
   });
 };
