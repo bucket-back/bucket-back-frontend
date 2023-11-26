425d2280be7e7e5d5fa45a7ab976fc8f44916d8c
daeun <gbajs754@gmail.com>
2023-11-27 01:25:00 +0900

    refactor: 인벤토리 홈, 상세페이지 수정

---
+++ src/features/hobby/components/HobbySelector/index.tsx
@@ -0,0 +1,58 @@
+import { useQuery } from '@tanstack/react-query';
+import { CommonRadio } from '@/shared/components';
+import { hobbyQueryOption } from '../../service';
+import { Container } from './style';
+
+interface Hobby {
+  english: string;
+  hangul: string;
+}
+
+interface HobbyRadioProps {
+  defaultValue?: string;
+  onChange?: React.Dispatch<React.SetStateAction<Hobby>>;
+  isReadOnly?: boolean;
+}
+
+const HobbySelector = ({ defaultValue, onChange, isReadOnly }: HobbyRadioProps) => {
+  const { data, isPending, isError } = useQuery({
+    ...hobbyQueryOption.all(),
+    select: (data) =>
+      data.hobbies.reduce<Record<string, string>>(
+        (acc, cur) => ((acc[cur.value] = cur.name), acc),
+        {}
+      ),
+  });
+
+  if (isPending) {
+    return;
+  }
+
+  if (isError) {
+    return;
+  }
+
+  const hangulHobby = Object.keys(data);
+
+  const handleChange = (value: string) => {
+    onChange &&
+      onChange({
+        english: data[value],
+        hangul: value,
+      });
+  };
+
+  return (
+    <Container>
+      <CommonRadio
+        values={hangulHobby}
+        name="취미"
+        onChange={(value: string) => handleChange(value)}
+        defaultValue={defaultValue}
+        isReadOnly={isReadOnly}
+      />
+    </Container>
+  );
+};
+
+export default HobbySelector;
--- src/pages/Inventory/InventoryDetail/index.tsx
+++ src/pages/Inventory/InventoryDetail/index.tsx
@@ -1,72 +1,50 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { CommonDrawer, CommonImage, CommonMenu, CommonText, Header } from '@/shared/components';
 import { useDrawer } from '@/shared/hooks';
+import { formatNumber } from '@/shared/utils';
 import { Container, ContentWrapper, Grid, GridItem, TitleWrapper } from './style';
 import { useHobby } from '@/features/hobby/hooks';
 import InventorySelectItem from '@/features/inventory/components/InventorySelectItem';
 import { useUpdateInventory } from '@/features/inventory/hook';
 import useDeleteInventory from '@/features/inventory/hook/useDeleteInventory';
-import { inventoryQueryOption } from '@/features/inventory/service';
-
-interface SelectedItem {
-  id: string;
-  src: string;
-}
+import { SelectedItem, inventoryQueryOption } from '@/features/inventory/service';
 
 const InventoryDetail = () => {
-  const { nickname, inventoryId } = useParams();
+  const { nickname, inventoryId } = useParams() as { nickname: string; inventoryId: string };
+  const numberInventoryId = Number(inventoryId);
+  const [selectedItems, setSelectedItems] = useState<SelectedItem[]>([]);
+  const { isOpen, onOpen, onClose } = useDrawer();
   const { data: hobbyData } = useHobby();
   const { data: inventoryDetailData } = useQuery({
     ...inventoryQueryOption.detail({
-      nickname: String(nickname),
-      inventoryId: Number(inventoryId),
+      nickname: nickname,
+      inventoryId: numberInventoryId,
     }),
   });
   const { mutate: deleteInventoryMutate } = useDeleteInventory();
   const currentHobby = hobbyData?.hobbies.find(({ value }) => value === inventoryDetailData?.hobby);
-  const { data: myItemsData } = useQuery({
-    ...inventoryQueryOption.myItems({
-      hobbyName: currentHobby?.name,
-      inventoryId: Number(inventoryId),
-    }),
-  });
   const { mutate: updateInventoryMutate } = useUpdateInventory({
-    nickname: String(nickname),
-    inventoryId: Number(inventoryId),
+    nickname: nickname,
+    inventoryId: numberInventoryId,
     hobbyName: String(currentHobby?.name),
   });
-  const updatedItems =
-    myItemsData?.reviewedItems
-      .filter((item) => item.isSelected)
-      .map((item) => {
-        return { id: String(item.itemInfo.id), src: item.itemInfo.image };
-      }) || [];
-  const [selectedItems, setSelectedItems] = useState<SelectedItem[]>([]);
-
-  const { isOpen, onOpen, onClose } = useDrawer();
 
   const handleDeleteInventory = () => {
-    // TODO : 정말 삭제할건지 모달띄워주기
-    deleteInventoryMutate(Number(inventoryId));
-  };
-
-  const checkingItems = (e: ChangeEvent<HTMLInputElement>, src: string) => {
-    const checked = e.target.checked;
-    if (checked) {
-      setSelectedItems(() => [...selectedItems, { id: e.target.id, src: src }]);
-    } else if (!checked) {
-      setSelectedItems(selectedItems.filter(({ id }) => id !== e.target.id));
-    }
+    // TODO: 정말 삭제할건지 모달띄워주기
+    deleteInventoryMutate(numberInventoryId);
   };
 
   const onSubmit = () => {
-    const itemIds = selectedItems.map((item) => Number(item.id));
-    updateInventoryMutate({
-      inventoryId: Number(inventoryId),
-      itemIds,
-    });
+    const itemIds = selectedItems.map((item) => item.id);
+    const prevItemIds = inventoryDetailData?.inventoryItemInfos.map((item) => item.id);
+    const isSameItems = JSON.stringify(prevItemIds) === JSON.stringify(itemIds);
+    !isSameItems &&
+      updateInventoryMutate({
+        inventoryId: numberInventoryId,
+        itemIds,
+      });
   };
 
   return (
@@ -81,7 +59,6 @@ const InventoryDetail = () => {
             onDelete={handleDeleteInventory}
             onUpdate={() => {
               onOpen();
-              setSelectedItems([...updatedItems]);
             }}
           />
         </TitleWrapper>
@@ -97,7 +74,7 @@ const InventoryDetail = () => {
             <GridItem key={index}>
               <CommonImage size="sm" src={image} />
               <CommonText type="smallInfo">{name}</CommonText>
-              <CommonText type="smallInfo">{price}</CommonText>
+              <CommonText type="smallInfo">{formatNumber(price)}</CommonText>
             </GridItem>
           ))}
         </Grid>
@@ -116,7 +93,12 @@ const InventoryDetail = () => {
         footerButtonText="수정 완료"
         isDisabled={selectedItems.length < 1}
       >
-        <InventorySelectItem reviewedItems={myItemsData?.reviewedItems} onChange={checkingItems} />
+        <InventorySelectItem
+          selectedHobby={currentHobby?.name || ''}
+          onChange={setSelectedItems}
+          selectedItems={selectedItems}
+          inventoryId={inventoryId}
+        />
       </CommonDrawer>
     </>
   );
