2946aa6ab3326115c21bb2b79ca2d8f4636e670d
kim <perfumeplaylist.gmail.com>
2023-11-27 16:41:55 +0900

    feat: 검색 결과 api 연결

---
+++ src/features/search/components/searchItemList/index.tsx
@@ -0,0 +1,60 @@
+import { useNavigate } from 'react-router-dom';
+import { Grid, GridItem } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import {
+  CommonButton,
+  CommonDivider,
+  CommonIcon,
+  CommonImage,
+  CommonText,
+} from '@/shared/components';
+import { formatNumber } from '@/shared/utils';
+import { Wrapper, Box, TextBox } from './style';
+import { searchQueryOption } from '@/features/search/service';
+
+const SearchItemList = () => {
+  const { data, isPending, isError } = useQuery({
+    ...searchQueryOption.itemList({ keyword: '농구', size: 10 }),
+  });
+
+  const navigate = useNavigate();
+
+  if (isPending) {
+    return <>Loading...</>;
+  }
+
+  if (isError) {
+    return <>Error...</>;
+  }
+
+  return data.totalCount >= 1 ? (
+    <>
+      <Box>
+        <TextBox>
+          <CommonText type="subStrongInfo">총 {data.totalCount}개의 아이템</CommonText>
+        </TextBox>
+        <Grid padding="0 1rem" templateColumns="repeat(3,1fr)" gap="0.25rem">
+          {data.items.map(({ itemSummary }) => (
+            <GridItem key={itemSummary.id} onClick={() => navigate(`/item/${itemSummary.id}`)}>
+              <CommonImage size="sm" src={itemSummary.image} />
+              <CommonText type="normalInfo">{formatNumber(itemSummary.price)}원</CommonText>
+              <CommonText type="smallInfo">{itemSummary.name}</CommonText>
+            </GridItem>
+          ))}
+        </Grid>
+        <CommonDivider size="sm" />
+        <div>
+          <CommonText type="smallInfo">원하시는 아이템이 없나요?</CommonText>
+          <Wrapper onClick={() => navigate('/item/create')}>
+            <CommonButton type="text">아이템 추가하러가기</CommonButton>
+            <CommonIcon type="chevronRight" />
+          </Wrapper>
+        </div>
+      </Box>
+    </>
+  ) : (
+    <>검색결과가 없습니다.</>
+  );
+};
+
+export default SearchItemList;
---
+++ src/features/search/components/searchVoteList/index.tsx
@@ -0,0 +1,81 @@
+import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { CommonButton, CommonCard, CommonIcon, CommonImage, CommonText } from '@/shared/components';
+import { formatNumber } from '@/shared/utils';
+import {
+  Wrapper,
+  ContentsContainer,
+  ContentsWrapper,
+  VoteImageWrapper,
+  VsBox,
+  Box,
+  TextBox,
+} from './style';
+import { searchQueryOption } from '@/features/search/service';
+const SearchVoteList = () => {
+  const { data, isPending, isError } = useQuery({
+    ...searchQueryOption.voteList({ keyword: '농구', size: 10 }),
+  });
+
+  const navigate = useNavigate();
+
+  if (isPending) {
+    return <>Loading...</>;
+  }
+
+  if (isError) {
+    return <>Error...</>;
+  }
+
+  return data.totalCount >= 1 ? (
+    <>
+      <Box>
+        <TextBox>
+          <CommonText type="subStrongInfo">총 {data.totalCount}개의 투표</CommonText>
+        </TextBox>
+        {data.votes.map(({ item1Info, item2Info, voteInfo }) => (
+          <CommonCard
+            count={3}
+            date={voteInfo.startTime}
+            onClick={() => {
+              navigate(`/vote/${voteInfo.id}`);
+            }}
+            key={voteInfo.id}
+          >
+            <ContentsContainer>
+              <CommonText type="smallInfo">{voteInfo.content}</CommonText>
+              <ContentsWrapper>
+                <VoteImageWrapper>
+                  <CommonImage size="base" src={item1Info.image} />
+                  <CommonText type="smallInfo">{formatNumber(item1Info.price)}</CommonText>
+                  <CommonText type="smallInfo">{item1Info.name}</CommonText>
+                </VoteImageWrapper>
+                <VsBox>
+                  <CommonText type="smallInfo" noOfLines={0}>
+                    VS
+                  </CommonText>
+                </VsBox>
+                <VoteImageWrapper>
+                  <CommonImage size="base" src={item2Info.image} />
+                  <CommonText type="smallInfo">{formatNumber(item2Info.price)}</CommonText>
+                  <CommonText type="smallInfo">{item2Info.name}</CommonText>
+                </VoteImageWrapper>
+              </ContentsWrapper>
+            </ContentsContainer>
+          </CommonCard>
+        ))}
+        <div>
+          <CommonText type="smallInfo">투표 검색결과 페이지가 없습니다!</CommonText>
+          <Wrapper onClick={() => navigate('/vote/:voteId')}>
+            <CommonButton type="text">투표 작성하러 가기</CommonButton>
+            <CommonIcon type="chevronRight" />
+          </Wrapper>
+        </div>
+      </Box>
+    </>
+  ) : (
+    <>검색결과가 없습니다...</>
+  );
+};
+
+export default SearchVoteList;
