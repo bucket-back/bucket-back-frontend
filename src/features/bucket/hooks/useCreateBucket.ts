dc433539e10650b973ab75f27b29748fa2b3f725
seeyoujeong <chan3358@naver.com>
2023-11-27 22:31:26 +0900

    feat: 에러 메시지 토스트 추가

--- src/features/bucket/hooks/useCreateBucket.ts
+++ src/features/bucket/hooks/useCreateBucket.ts
@@ -1,7 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
 import { useCustomToast } from '@/shared/hooks';
 import { bucketApi } from '../service';
+
+interface ErrorData {
+  code: string;
+  error: string;
+  message: string;
+}
 
 const useCreateBucket = () => {
   const openToast = useCustomToast();
@@ -12,6 +19,9 @@ const useCreateBucket = () => {
     onSuccess: () => {
       openToast({ message: '버킷을 생성했습니다.', type: 'success' });
       navigate(-1);
+    },
+    onError: ({ data }: AxiosResponse<ErrorData>) => {
+      openToast({ message: data.message, type: 'error' });
     },
   });
 };
