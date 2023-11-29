197758c449f010aab257f9a747367247a9b482d1
kim <perfumeplaylist.gmail.com>
2023-11-30 02:59:43 +0900

    feat: search 무한스크롤 적용

--- src/features/search/components/searchItemList/index.tsx
+++ src/features/search/components/searchItemList/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { Grid, GridItem } from '@chakra-ui/react';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import {
   CommonButton,
   CommonDivider,
@@ -8,6 +8,7 @@ import {
   CommonImage,
   CommonText,
 } from '@/shared/components';
+import { useIntersectionObserver } from '@/shared/hooks';
 import { formatNumber } from '@/shared/utils';
 import { Wrapper, Box, TextBox, NoResult } from './style';
 import { searchQueryOption } from '@/features/search/service';
@@ -18,9 +19,11 @@ export interface SearchListItemProp {
 }
 
 const SearchItemList = ({ keyword }: SearchListItemProp) => {
-  const { data, isPending, isError } = useQuery({
-    ...searchQueryOption.itemList({ keyword, size: 10 }),
+  const { data, isPending, isError, hasNextPage, fetchNextPage } = useInfiniteQuery({
+    ...searchQueryOption.infiniteKeywordItemList({ keyword: encodeURIComponent(keyword), size: 3 }),
   });
+
+  const ref = useIntersectionObserver({ onObserve: fetchNextPage });
 
   const navigate = useNavigate();
 
@@ -32,20 +35,31 @@ const SearchItemList = ({ keyword }: SearchListItemProp) => {
     return <>Error...</>;
   }
 
-  return data.totalCount >= 1 ? (
+  if (!data.pages[0].totalCount) {
+    return <NoResult>검색결과가 없습니다.</NoResult>;
+  }
+
+  const totalCount = data.pages
+    .map(({ totalCount }) => totalCount)
+    .reduce((prev, next) => prev + next, 0);
+
+  return (
     <>
       <Box>
         <TextBox>
-          <CommonText type="subStrongInfo">총 {data.totalCount}개의 아이템</CommonText>
+          <CommonText type="subStrongInfo">총 {totalCount}개의 아이템</CommonText>
         </TextBox>
         <Grid padding="0 1rem" templateColumns="repeat(3,1fr)" gap="0.25rem">
-          {data.items.map(({ itemSummary }) => (
-            <GridItem key={itemSummary.id} onClick={() => navigate(`/item/${itemSummary.id}`)}>
-              <CommonImage size="sm" src={itemSummary.image} />
-              <CommonText type="normalInfo">{formatNumber(itemSummary.price)}원</CommonText>
-              <CommonText type="smallInfo">{itemSummary.name}</CommonText>
-            </GridItem>
-          ))}
+          {data.pages.map(({ items }) =>
+            items.map(({ itemSummary }) => (
+              <GridItem key={itemSummary.id} onClick={() => navigate(`/item/${itemSummary.id}`)}>
+                <CommonImage size="sm" src={itemSummary.image} />
+                <CommonText type="normalInfo">{formatNumber(itemSummary.price)}원</CommonText>
+                <CommonText type="smallInfo">{itemSummary.name}</CommonText>
+              </GridItem>
+            ))
+          )}
+          {hasNextPage && <div ref={ref} />}
         </Grid>
         <CommonDivider size="sm" />
         <div>
@@ -57,8 +71,6 @@ const SearchItemList = ({ keyword }: SearchListItemProp) => {
         </div>
       </Box>
     </>
-  ) : (
-    <NoResult>검색결과가 없습니다.</NoResult>
   );
 };
 
--- src/features/search/components/searchVoteList/index.tsx
+++ src/features/search/components/searchVoteList/index.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { CommonButton, CommonCard, CommonIcon, CommonImage, CommonText } from '@/shared/components';
+import { useIntersectionObserver } from '@/shared/hooks';
 import { formatNumber } from '@/shared/utils';
 import { SearchListItemProp } from '../searchItemList';
 import {
@@ -16,11 +17,13 @@ import {
 import { searchQueryOption } from '@/features/search/service';
 
 const SearchVoteList = ({ keyword }: SearchListItemProp) => {
-  const { data, isPending, isError } = useQuery({
-    ...searchQueryOption.voteList({ keyword, size: 10 }),
+  const { data, isPending, isError, hasNextPage, fetchNextPage } = useInfiniteQuery({
+    ...searchQueryOption.infiniteVoteList({ keyword: encodeURIComponent(keyword), size: 3 }),
   });
 
   const navigate = useNavigate();
+
+  const ref = useIntersectionObserver({ onObserve: fetchNextPage });
 
   if (isPending) {
     return <>Loading...</>;
@@ -30,54 +33,65 @@ const SearchVoteList = ({ keyword }: SearchListItemProp) => {
     return <>Error...</>;
   }
 
-  return data.totalCount >= 1 ? (
+  if (!data.pages[0].totalCount) {
+    return <NoResult>검색결과가 없습니다...</NoResult>;
+  }
+
+  const totalCount = data.pages
+    .map(({ totalCount }) => totalCount)
+    .reduce((prev, next) => prev + next, 0);
+
+  return (
     <>
       <Box>
         <TextBox>
-          <CommonText type="subStrongInfo">총 {data.totalCount}개의 투표</CommonText>
+          <CommonText type="subStrongInfo">총 {totalCount}개의 투표</CommonText>
         </TextBox>
-        {data.votes.map(({ item1Info, item2Info, voteInfo }) => (
-          <CommonCard
-            count={3}
-            date={voteInfo.startTime}
-            onClick={() => {
-              navigate(`/vote/${voteInfo.id}`);
-            }}
-            key={voteInfo.id}
-          >
-            <ContentsContainer>
-              <CommonText type="smallInfo">{voteInfo.content}</CommonText>
-              <ContentsWrapper>
-                <VoteImageWrapper>
-                  <CommonImage size="base" src={item1Info.image} />
-                  <CommonText type="smallInfo">{formatNumber(item1Info.price)}</CommonText>
-                  <CommonText type="smallInfo">{item1Info.name}</CommonText>
-                </VoteImageWrapper>
-                <VsBox>
-                  <CommonText type="smallInfo" noOfLines={0}>
-                    VS
-                  </CommonText>
-                </VsBox>
-                <VoteImageWrapper>
-                  <CommonImage size="base" src={item2Info.image} />
-                  <CommonText type="smallInfo">{formatNumber(item2Info.price)}</CommonText>
-                  <CommonText type="smallInfo">{item2Info.name}</CommonText>
-                </VoteImageWrapper>
-              </ContentsWrapper>
-            </ContentsContainer>
-          </CommonCard>
-        ))}
+        <>
+          {data.pages.map(({ votes }) =>
+            votes.map(({ item1Info, item2Info, voteInfo }) => (
+              <CommonCard
+                count={3}
+                date={voteInfo.startTime}
+                onClick={() => {
+                  navigate(`/vote/${voteInfo.id}`);
+                }}
+                key={voteInfo.id}
+              >
+                <ContentsContainer>
+                  <CommonText type="smallInfo">{voteInfo.content}</CommonText>
+                  <ContentsWrapper>
+                    <VoteImageWrapper>
+                      <CommonImage size="base" src={item1Info.image} />
+                      <CommonText type="smallInfo">{formatNumber(item1Info.price)}</CommonText>
+                      <CommonText type="smallInfo">{item1Info.name}</CommonText>
+                    </VoteImageWrapper>
+                    <VsBox>
+                      <CommonText type="smallInfo" noOfLines={0}>
+                        VS
+                      </CommonText>
+                    </VsBox>
+                    <VoteImageWrapper>
+                      <CommonImage size="base" src={item2Info.image} />
+                      <CommonText type="smallInfo">{formatNumber(item2Info.price)}</CommonText>
+                      <CommonText type="smallInfo">{item2Info.name}</CommonText>
+                    </VoteImageWrapper>
+                  </ContentsWrapper>
+                </ContentsContainer>
+              </CommonCard>
+            ))
+          )}
+          {hasNextPage && <div ref={ref} />}
+        </>
         <div>
           <CommonText type="smallInfo">투표 검색결과 페이지가 없습니다!</CommonText>
-          <Wrapper onClick={() => navigate('/vote/:voteId')}>
+          <Wrapper onClick={() => navigate('/vote/create')}>
             <CommonButton type="text">투표 작성하러 가기</CommonButton>
             <CommonIcon type="chevronRight" />
           </Wrapper>
         </div>
       </Box>
     </>
-  ) : (
-    <NoResult>검색결과가 없습니다...</NoResult>
   );
 };
 
