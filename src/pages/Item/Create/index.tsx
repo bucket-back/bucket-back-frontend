92abf2854a430093fd9841e89f074c6e52b9b938
kim <perfumeplaylist.gmail.com>
2023-11-29 18:32:00 +0900

    fix: placeholder 상수처리

--- src/pages/Item/Create/index.tsx
+++ src/pages/Item/Create/index.tsx
@@ -12,6 +12,8 @@ interface ItemText {
 const validateInput = (v: string) =>
   !(v.includes('<script>') || v.includes('</script>')) || '스크립트 태그를 사용할수 없습니다';
 
+const PLACEHOLDERURL =
+  'https://www.coupang.com/vp/products/7694535500?itemId=20584722958&vendorItemId=87659712741&sourceType=srp_product_ads&clickEventId=6717a120-87bb-11ee-9cee-15b675c1dd9d&korePlacement=15&koreSubPlacement=11&q=%EB%86%8D%EA%B5%AC&itemsCount=36&searchId=bbc4652630a74878a83e215b18e6ca01&rank=10&isAddedCart=';
 const ItemCreate = () => {
   const {
     register,
@@ -58,7 +60,7 @@ const ItemCreate = () => {
               <Box>
                 <CommonInput
                   onInput={() => setShowMessage(false)}
-                  placeholder="https://www.coupang.com/vp/products/7694535500?itemId=20584722958&vendorItemId=87659712741&sourceType=srp_product_ads&clickEventId=6717a120-87bb-11ee-9cee-15b675c1dd9d&korePlacement=15&koreSubPlacement=11&q=%EB%86%8D%EA%B5%AC&itemsCount=36&searchId=bbc4652630a74878a83e215b18e6ca01&rank=10&isAddedCart="
+                  placeholder={PLACEHOLDERURL}
                   type="text"
                   width="full"
                   label="아이템을 추가할 URL을 입력해주세요"
