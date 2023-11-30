3d9a380a6aba50ae9ce0a5a494c776eee5129004
kim <perfumeplaylist.gmail.com>
2023-11-30 13:35:15 +0900

    refactor: flat한 데이터로 커스텀

--- src/features/search/components/searchVoteList/index.tsx
+++ src/features/search/components/searchVoteList/index.tsx
@@ -19,6 +19,12 @@ import { searchQueryOption } from '@/features/search/service';
 const SearchVoteList = ({ keyword }: SearchListItemProp) => {
   const { data, isPending, isError, hasNextPage, fetchNextPage } = useInfiniteQuery({
     ...searchQueryOption.infiniteVoteList({ keyword: encodeURIComponent(keyword), size: 3 }),
+    select: (data) => {
+      return {
+        totalCount: data.pages.flatMap(({ totalCount }) => totalCount),
+        votes: data.pages.flatMap(({ votes }) => votes),
+      };
+    },
   });
 
   const navigate = useNavigate();
@@ -33,13 +39,11 @@ const SearchVoteList = ({ keyword }: SearchListItemProp) => {
     return <>Error...</>;
   }
 
-  if (!data.pages[0].totalCount) {
+  if (data.totalCount[0] === 0) {
     return <NoResult>검색결과가 없습니다...</NoResult>;
   }
 
-  const totalCount = data.pages
-    .map(({ totalCount }) => totalCount)
-    .reduce((prev, next) => prev + next, 0);
+  const totalCount = data.totalCount.reduce((prev, next) => prev + next, 0);
 
   return (
     <>
@@ -48,39 +52,37 @@ const SearchVoteList = ({ keyword }: SearchListItemProp) => {
           <CommonText type="subStrongInfo">총 {totalCount}개의 투표</CommonText>
         </TextBox>
         <>
-          {data.pages.map(({ votes }) =>
-            votes.map(({ item1Info, item2Info, voteInfo }) => (
-              <CommonCard
-                count={3}
-                date={voteInfo.startTime}
-                onClick={() => {
-                  navigate(`/vote/${voteInfo.id}`);
-                }}
-                key={voteInfo.id}
-              >
-                <ContentsContainer>
-                  <CommonText type="smallInfo">{voteInfo.content}</CommonText>
-                  <ContentsWrapper>
-                    <VoteImageWrapper>
-                      <CommonImage size="base" src={item1Info.image} />
-                      <CommonText type="smallInfo">{formatNumber(item1Info.price)}</CommonText>
-                      <CommonText type="smallInfo">{item1Info.name}</CommonText>
-                    </VoteImageWrapper>
-                    <VsBox>
-                      <CommonText type="smallInfo" noOfLines={0}>
-                        VS
-                      </CommonText>
-                    </VsBox>
-                    <VoteImageWrapper>
-                      <CommonImage size="base" src={item2Info.image} />
-                      <CommonText type="smallInfo">{formatNumber(item2Info.price)}</CommonText>
-                      <CommonText type="smallInfo">{item2Info.name}</CommonText>
-                    </VoteImageWrapper>
-                  </ContentsWrapper>
-                </ContentsContainer>
-              </CommonCard>
-            ))
-          )}
+          {data.votes.map(({ item1Info, item2Info, voteInfo }) => (
+            <CommonCard
+              count={3}
+              date={voteInfo.startTime}
+              onClick={() => {
+                navigate(`/vote/${voteInfo.id}`);
+              }}
+              key={voteInfo.id}
+            >
+              <ContentsContainer>
+                <CommonText type="smallInfo">{voteInfo.content}</CommonText>
+                <ContentsWrapper>
+                  <VoteImageWrapper>
+                    <CommonImage size="base" src={item1Info.image} />
+                    <CommonText type="smallInfo">{formatNumber(item1Info.price)}</CommonText>
+                    <CommonText type="smallInfo">{item1Info.name}</CommonText>
+                  </VoteImageWrapper>
+                  <VsBox>
+                    <CommonText type="smallInfo" noOfLines={0}>
+                      VS
+                    </CommonText>
+                  </VsBox>
+                  <VoteImageWrapper>
+                    <CommonImage size="base" src={item2Info.image} />
+                    <CommonText type="smallInfo">{formatNumber(item2Info.price)}</CommonText>
+                    <CommonText type="smallInfo">{item2Info.name}</CommonText>
+                  </VoteImageWrapper>
+                </ContentsWrapper>
+              </ContentsContainer>
+            </CommonCard>
+          ))}
           {hasNextPage && <div ref={ref} />}
         </>
         <div>
