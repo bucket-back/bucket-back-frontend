799684fbfe44dbfa1ad82be3e998ddfb541e5d71
kim <perfumeplaylist.gmail.com>
2023-12-01 13:21:10 +0900

    style: gap 추가

--- src/pages/Search/SearchMain/style.tsx
+++ src/pages/Search/SearchMain/style.tsx
@@ -4,7 +4,7 @@ export const Container = styled.article`
   padding: 1.75rem;
   display: flex;
   flex-direction: column;
-  gap: 1rem 0;
+  gap: 2rem 0;
 `;
 
 export const Wrapper = styled.section`
