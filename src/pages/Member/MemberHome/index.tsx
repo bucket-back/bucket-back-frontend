c95d19a79fb18e94bb408c71b04621361329bf73
seeyoujeong <chan3358@naver.com>
2023-12-01 15:10:59 +0900

    fix: 마이페이지 짤리는 오류 및 자기소개 글자 제한 수정

--- src/pages/Member/MemberHome/index.tsx
+++ src/pages/Member/MemberHome/index.tsx
@@ -87,93 +87,95 @@ const MemberHome = () => {
           </CommonText>
         </MemberIntroWrapper>
         <CommonDivider size="lg" />
-        <ContentsWrapper onClick={() => navigate(`/member/${nickname}/inventory`)}>
-          <ContentsPanel>
-            <div>
-              <SubTitleBox>
-                <CommonIcon type="inventory" />
-                <CommonText type="smallTitle">인벤토리</CommonText>
-              </SubTitleBox>
-              <CommonText type="smallInfo">
-                취미별 아이템을 조합하여 나만의 인벤토리를 만들 수 있어요
-              </CommonText>
-            </div>
-            <IconBox>
-              <CommonIcon type="chevronRight" size="0.8rem" />
-            </IconBox>
-          </ContentsPanel>
-          {member.isSuccess && member.data.inventoryProfiles.length > 0 ? (
-            <ImagePanel>
-              <Grid>
-                {member.data.inventoryProfiles.map((inventory) => (
-                  <DividerImage key={inventory.id} type="base" images={inventory.images} />
-                ))}
-              </Grid>
-            </ImagePanel>
-          ) : (
-            isSelf && (
-              <AddBox>
-                <CommonText type="smallInfo">보유하신 인벤토리가 없네요.</CommonText>
-                <CommonButton type="profile" onClick={() => navigate('/inventory/create')}>
-                  인벤토리 추가하러가기
-                </CommonButton>
-              </AddBox>
-            )
-          )}
-        </ContentsWrapper>
-        <CommonDivider size="sm" />
-        <ContentsWrapper onClick={() => navigate(`/member/${nickname}/bucket`)}>
-          <ContentsPanel>
-            <div>
-              <SubTitleBox>
-                <CommonIcon type="bucket" />
-                <CommonText type="smallTitle">버킷</CommonText>
-              </SubTitleBox>
-              <CommonText type="smallInfo">
-                취미별 아이템을 조합하여 나만의 버킷을 만들 수 있어요
-              </CommonText>
-            </div>
-            <IconBox>
-              <CommonIcon type="chevronRight" size="0.8rem" />
-            </IconBox>
-          </ContentsPanel>
-          {member.isSuccess && member.data.bucketProfiles.length > 0 ? (
-            <ImagePanel>
-              <Grid>
-                {member.data?.bucketProfiles.map((bucket) => (
-                  <DividerImage key={bucket.id} type="base" images={bucket.images} />
-                ))}
-              </Grid>
-            </ImagePanel>
-          ) : (
-            isSelf && (
-              <AddBox>
-                <CommonText type="smallInfo">보유하신 버킷이 없어요.</CommonText>
-                <CommonButton type="profile" onClick={() => navigate('/bucket/create')}>
-                  버킷 추가하러가기
-                </CommonButton>
-              </AddBox>
-            )
-          )}
-        </ContentsWrapper>
-        <CommonDivider size="sm" />
-        <ContentsWrapper onClick={() => navigate(`/member/${nickname}/feed`)}>
-          <ContentsPanel>
-            <div>
-              <SubTitleBox>
-                <CommonIcon type="feed" />
-                <CommonText type="smallTitle">피드</CommonText>
-              </SubTitleBox>
-              <CommonText type="smallInfo">
-                내가 올린 피드와 좋아요한 피드를 확인할 수 있어요
-              </CommonText>
-            </div>
-            <IconBox>
-              <CommonIcon type="chevronRight" size="0.8rem" />
-            </IconBox>
-          </ContentsPanel>
-        </ContentsWrapper>
-        <CommonDivider size="sm" />
+        <div style={{ height: '100%', overflowY: 'scroll' }}>
+          <ContentsWrapper onClick={() => navigate(`/member/${nickname}/inventory`)}>
+            <ContentsPanel>
+              <div>
+                <SubTitleBox>
+                  <CommonIcon type="inventory" />
+                  <CommonText type="smallTitle">인벤토리</CommonText>
+                </SubTitleBox>
+                <CommonText type="smallInfo">
+                  취미별 아이템을 조합하여 나만의 인벤토리를 만들 수 있어요
+                </CommonText>
+              </div>
+              <IconBox>
+                <CommonIcon type="chevronRight" size="0.8rem" />
+              </IconBox>
+            </ContentsPanel>
+            {member.isSuccess && member.data.inventoryProfiles.length > 0 ? (
+              <ImagePanel>
+                <Grid>
+                  {member.data.inventoryProfiles.map((inventory) => (
+                    <DividerImage key={inventory.id} type="base" images={inventory.images} />
+                  ))}
+                </Grid>
+              </ImagePanel>
+            ) : (
+              isSelf && (
+                <AddBox>
+                  <CommonText type="smallInfo">보유하신 인벤토리가 없네요.</CommonText>
+                  <CommonButton type="profile" onClick={() => navigate('/inventory/create')}>
+                    인벤토리 추가하러가기
+                  </CommonButton>
+                </AddBox>
+              )
+            )}
+          </ContentsWrapper>
+          <CommonDivider size="sm" />
+          <ContentsWrapper onClick={() => navigate(`/member/${nickname}/bucket`)}>
+            <ContentsPanel>
+              <div>
+                <SubTitleBox>
+                  <CommonIcon type="bucket" />
+                  <CommonText type="smallTitle">버킷</CommonText>
+                </SubTitleBox>
+                <CommonText type="smallInfo">
+                  취미별 아이템을 조합하여 나만의 버킷을 만들 수 있어요
+                </CommonText>
+              </div>
+              <IconBox>
+                <CommonIcon type="chevronRight" size="0.8rem" />
+              </IconBox>
+            </ContentsPanel>
+            {member.isSuccess && member.data.bucketProfiles.length > 0 ? (
+              <ImagePanel>
+                <Grid>
+                  {member.data?.bucketProfiles.map((bucket) => (
+                    <DividerImage key={bucket.id} type="base" images={bucket.images} />
+                  ))}
+                </Grid>
+              </ImagePanel>
+            ) : (
+              isSelf && (
+                <AddBox>
+                  <CommonText type="smallInfo">보유하신 버킷이 없어요.</CommonText>
+                  <CommonButton type="profile" onClick={() => navigate('/bucket/create')}>
+                    버킷 추가하러가기
+                  </CommonButton>
+                </AddBox>
+              )
+            )}
+          </ContentsWrapper>
+          <CommonDivider size="sm" />
+          <ContentsWrapper onClick={() => navigate(`/member/${nickname}/feed`)}>
+            <ContentsPanel>
+              <div>
+                <SubTitleBox>
+                  <CommonIcon type="feed" />
+                  <CommonText type="smallTitle">피드</CommonText>
+                </SubTitleBox>
+                <CommonText type="smallInfo">
+                  내가 올린 피드와 좋아요한 피드를 확인할 수 있어요
+                </CommonText>
+              </div>
+              <IconBox>
+                <CommonIcon type="chevronRight" size="0.8rem" />
+              </IconBox>
+            </ContentsPanel>
+          </ContentsWrapper>
+          <CommonDivider size="sm" />
+        </div>
       </Container>
       <Footer />
 
