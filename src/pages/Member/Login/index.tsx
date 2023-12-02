565bd7c74fc42d710de75ffccd1c5a6771cb159b
daeun <gbajs754@gmail.com>
2023-12-02 16:43:21 +0900

    refactor: 로그인페이지 뒤로가기

--- src/pages/Member/Login/index.tsx
+++ src/pages/Member/Login/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import { CommonButton, CommonIcon, CommonInput } from '@/shared/components';
+import { CommonButton, CommonIcon, CommonInput, Header } from '@/shared/components';
 import { useValidateForm } from '@/shared/hooks';
 import { ButtonWrapper, Container, Form, IconWrapper, InputWrapper } from './style';
 import { useLogin } from '@/features/member/hooks';
@@ -23,44 +23,47 @@ const Login = () => {
   };
 
   return (
-    <Container>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <InputWrapper>
-          <CommonInput
-            type="text"
-            label="이메일"
-            placeholder="이메일을 입력해주세요."
-            error={errors.email}
-            {...register('email', ...registerOptions.email)}
-          />
-          <CommonInput
-            type={showPassword ? 'text' : 'password'}
-            label="비밀번호"
-            placeholder="비밀번호를 입력해주세요."
-            error={errors.password}
-            rightIcon={
-              <IconWrapper onClick={() => setShowPassword(() => !showPassword)}>
-                <CommonIcon type={showPassword ? 'eye' : 'eyeSlash'} size="1.25rem" />
-              </IconWrapper>
-            }
-            {...register('password', { required: '비밀번호를 입력해주세요.' })}
-          />
-        </InputWrapper>
-        <ButtonWrapper>
-          <CommonButton type="mdMiddle" isSubmit={true}>
-            로그인
-          </CommonButton>
-          <CommonButton
-            type="mdMiddle"
-            onClick={() => {
-              navigate('/signup');
-            }}
-          >
-            회원가입
-          </CommonButton>
-        </ButtonWrapper>
-      </Form>
-    </Container>
+    <>
+      <Header type="back" />
+      <Container>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <InputWrapper>
+            <CommonInput
+              type="text"
+              label="이메일"
+              placeholder="이메일을 입력해주세요."
+              error={errors.email}
+              {...register('email', ...registerOptions.email)}
+            />
+            <CommonInput
+              type={showPassword ? 'text' : 'password'}
+              label="비밀번호"
+              placeholder="비밀번호를 입력해주세요."
+              error={errors.password}
+              rightIcon={
+                <IconWrapper onClick={() => setShowPassword(() => !showPassword)}>
+                  <CommonIcon type={showPassword ? 'eye' : 'eyeSlash'} size="1.25rem" />
+                </IconWrapper>
+              }
+              {...register('password', { required: '비밀번호를 입력해주세요.' })}
+            />
+          </InputWrapper>
+          <ButtonWrapper>
+            <CommonButton type="mdMiddle" isSubmit={true}>
+              로그인
+            </CommonButton>
+            <CommonButton
+              type="mdMiddle"
+              onClick={() => {
+                navigate('/signup');
+              }}
+            >
+              회원가입
+            </CommonButton>
+          </ButtonWrapper>
+        </Form>
+      </Container>
+    </>
   );
 };
 
