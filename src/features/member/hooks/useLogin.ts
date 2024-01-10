a72b2cd527602acb230badcd3dba823c7eed8fd9
Chan Uk Jeong <40534414+seeyoujeong@users.noreply.github.com>
2024-01-10 15:56:51 +0900

    feat: token 키 이름 및 타입 수정 (#212)

--- src/features/member/hooks/useLogin.ts
+++ src/features/member/hooks/useLogin.ts
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
+import { TOKEN_KEY } from '@/shared/constants';
 import { useCustomToast } from '@/shared/hooks';
 import { Storage } from '@/shared/utils';
 import memberApi from '../service/handler';
@@ -11,12 +12,12 @@ const useLogin = () => {
   return useMutation({
     mutationFn: memberApi.postLogin,
     onSuccess: (data) => {
-      const { jwtToken, memberId, nickname } = data;
+      const { accessToken, memberId, nickname } = data;
       Storage.setLocalStoraged('userInfo', {
         memberId,
         nickname,
       });
-      Storage.setLocalStoraged('token', jwtToken);
+      Storage.setLocalStoraged(TOKEN_KEY, accessToken);
       openToast({ message: '로그인에 성공하셨습니다.', type: 'success' });
       navigate('/');
     },
