28c492b02242dda6ada8a6b10526370c47548d22
kim <perfumeplaylist.gmail.com>
2024-03-04 16:33:22 +0900

    chore: eslint jest recommend 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
     'plugin:react/recommended',
+    'plugin:jest/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
